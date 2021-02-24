eebd638888cd7163252ba243398b8c0ee2dd1201
dlasecki <darekshakur@gmail.com>
2021-02-24 14:58:09 +0100

    MolecularProblem unit test added with a transformer.

--- test/problems/second_quantization/molecular/test_molecular_problem.py
+++ test/problems/second_quantization/molecular/test_molecular_problem.py
@@ -13,8 +13,10 @@
 """Tests Molecular Problem."""
 
 from test import QiskitNatureTestCase
-from qiskit_nature import QiskitNatureError
-from qiskit_nature.drivers import PySCFDriver, UnitsType
+from qiskit_nature.problems.second_quantization.molecular.fermionic_op_builder import \
+    build_fermionic_op
+from qiskit_nature.transformers import ActiveSpaceTransformer
+from qiskit_nature.drivers import HDF5Driver
 from qiskit_nature.operators.second_quantization import SecondQuantizedOp
 from qiskit_nature.problems.second_quantization.molecular.molecular_problem import MolecularProblem
 
@@ -22,31 +24,48 @@ from qiskit_nature.problems.second_quantization.molecular.molecular_problem impo
 class TestMolecularProblem(QiskitNatureTestCase):
     """Tests Molecular Problem."""
 
-    def setUp(self):
-        super().setUp()
-        try:
-            self.driver = PySCFDriver(atom='Li .0 .0 .0; H .0 .0 1.595',
-                                      unit=UnitsType.ANGSTROM,
-                                      charge=0,
-                                      spin=0,
-                                      basis='sto3g')
-            transformation = []
-            self.molecular_problem = MolecularProblem(self.driver, transformation)
-
-        except QiskitNatureError:
-            self.skipTest('PYSCF driver does not appear to be installed')
-
-    def test_second_q_ops(self):
+    def test_second_q_ops_without_transformers(self):
         """Tests that the list of second quantized operators is created if no transformers
         provided."""
-        expected_num_of_electr_terms = 631
+        expected_num_of_electr_terms = 185
         expected_num_of_sec_quant_ops = 7
 
-        second_quantized_ops = self.molecular_problem.second_q_ops()
-        electronic_second_quantized_op = second_quantized_ops[0]
-        assert electronic_second_quantized_op.boson is None
-        assert electronic_second_quantized_op.spin == {}
+        driver = HDF5Driver(hdf5_input=self.get_resource_path('H2_631g.hdf5', 'transformers'))
+        transformation = []
+        molecular_problem = MolecularProblem(driver, transformation)
+
+        expected_electr_sec_quant_op = TestMolecularProblem._calc_expected_sec_quant_op(driver,
+                                                                                        None)
+
+        second_quantized_ops = molecular_problem.second_q_ops()
+        electr_sec_quant_op = second_quantized_ops[0]
+        assert electr_sec_quant_op.boson is None
+        assert electr_sec_quant_op.spin == {}
         assert len(second_quantized_ops) == expected_num_of_sec_quant_ops
         for second_quantized_op in second_quantized_ops:
             assert isinstance(second_quantized_op, SecondQuantizedOp)
-        assert len(electronic_second_quantized_op.fermion) == expected_num_of_electr_terms
+        assert len(electr_sec_quant_op.fermion) == expected_num_of_electr_terms
+        assert expected_electr_sec_quant_op.__eq__(electr_sec_quant_op)
+
+    def test_second_q_ops_with_active_space(self):
+        """Tests that the correct second quantized operator is created if an active space
+        transformer is provided."""
+        driver = HDF5Driver(hdf5_input=self.get_resource_path('H2_631g.hdf5', 'transformers'))
+        trafo = ActiveSpaceTransformer(num_electrons=2, num_orbitals=2)
+        expected_electr_sec_quant_op = TestMolecularProblem._calc_expected_sec_quant_op(driver,
+                                                                                        trafo)
+
+        molecular_problem = MolecularProblem(driver, [trafo])
+        second_quantized_ops_list = molecular_problem.second_q_ops()
+        electr_sec_quant_op = second_quantized_ops_list[0]
+
+        assert expected_electr_sec_quant_op.__eq__(electr_sec_quant_op)
+        # TODO test QMolecule itself if it is ever a field in MolecularProblem
+
+    @staticmethod
+    def _calc_expected_sec_quant_op(driver, trafo):
+        q_molecule = driver.run()
+        if trafo:
+            q_molecule = trafo.transform(q_molecule)
+        electronic_fermionic_op = build_fermionic_op(q_molecule)
+        return SecondQuantizedOp([electronic_fermionic_op])
