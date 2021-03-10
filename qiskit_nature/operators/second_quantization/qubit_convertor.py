0efc19e866aafd41b856dbd72dc52e4c9f89abd5
woodsp <woodsp@us.ibm.com>
2021-03-10 16:35:38 -0500

    Added convertor and mapping tests

---
+++ qiskit_nature/operators/second_quantization/qubit_convertor.py
@@ -0,0 +1,111 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""A convertor from Second-Quantized to Qubit Operators."""
+from typing import List, Optional, Tuple, Union
+
+from qiskit.opflow import PauliSumOp
+from qiskit.opflow.primitive_ops import Z2Symmetries
+
+from qiskit_nature import QiskitNatureError
+from qiskit_nature.mappers.second_quantization import QubitMapper
+
+from . import SecondQuantizedOp
+
+# TODO two qubit reduction flag for parity mapping; special case it here? Or add some flag to
+#      mappers that is generally false but can be set via parity mapper
+
+
+class QubitConvertor:
+    """A convertor from Second-Quantized to Qubit Operators."""
+
+    def __init__(self,
+                 mappers: Union[QubitMapper, Tuple[QubitMapper, QubitMapper]],
+                 z2symmetry_reduction: Optional[List[int]] = None):
+
+        self._mappers = mappers
+        self._z2symmetry_reduction = z2symmetry_reduction
+        self._z2symmetries = None
+
+    @property
+    def z2symmetry_reduction(self) -> Optional[List[int]]:
+        """Get z2symmetry_reduction"""
+        return self._z2symmetry_reduction
+
+    @z2symmetry_reduction.setter
+    def z2symmetry_reduction(self, z2symmetry_reduction: Optional[List[int]]) -> None:
+        """Set z2symmetry_reduction"""
+        self._z2symmetry_reduction = z2symmetry_reduction
+        self._z2symmetries = None
+
+    @property
+    def z2symmetries(self) -> Optional[Z2Symmetries]:
+        """Get z2symmetries. Will be `None` until :meth:`to_qubit_ops` has been called."""
+        return self._z2symmetries
+
+    def to_qubit_ops(self, second_q_ops: List[SecondQuantizedOp],
+                     z2symmetry_reduction: List[int] = None,
+                     ) -> List[Optional[PauliSumOp]]:
+        """
+        Maps the given list of second quantized operators to qubit operators. Also it will
+        carry out z2 symmetry reduction on the qubit operators if z2symmetry_reduction has
+        been specified.
+
+        Args:
+            second_q_ops: The list of second quantized operators to be converted
+            z2symmetry_reduction: Optional z2symmetry reduction, the sector of the symmetry
+
+        Returns:
+            A list of qubit operators or the same length as the second_q_ops list. The first
+            operator in the second_q_ops list is treated as the main operator and others must
+            commute with its symmetry, when symmetry reduction is being done. If it does not
+            then the position in the output list will be set to `None` to preserve the order.
+        """
+        if z2symmetry_reduction is not None:
+            self.z2symmetry_reduction = z2symmetry_reduction
+
+        qubit_ops = self._map_to_qubits(second_q_ops)
+        qubit_ops_reduced = self._symmetry_reduce(qubit_ops)
+
+        return qubit_ops_reduced
+
+    def _map_to_qubits(self, second_q_ops: List[SecondQuantizedOp]) -> List[PauliSumOp]:
+        if isinstance(self._mappers, tuple):
+            raise NotImplementedError
+
+        qubit_ops: List[PauliSumOp] = []
+        main_op = second_q_ops[0]
+        if main_op._fermion is not None:
+            for op in second_q_ops:
+                if op._fermion is None:
+                    raise QiskitNatureError("Second quantized operators must contain same type")
+                qubit_ops.append(self._mappers.map(op._fermion))
+        else:
+            raise QiskitNatureError("Cannot map invalid second quantized operator")
+
+        return qubit_ops
+
+    def _symmetry_reduce(self, qubit_ops: List[PauliSumOp]) -> List[Optional[PauliSumOp]]:
+
+        if self.z2symmetry_reduction is None:
+            reduced_qubit_ops = qubit_ops
+            self._z2symmetries = Z2Symmetries([], [], [], None)
+        else:
+            # If we are given the symmetry sector we can use it to reduce all operators.
+            # Otherwise if we are given a callable that can determine the right sector we
+            # will use that. Note this callable will most likely need problem related data
+            # such as info contained within MolecularProblem, so we need access in some way
+            # which needs to be determined.
+            reduced_qubit_ops = qubit_ops  # TODO actually reduce the operators...
+            self._z2symmetries = Z2Symmetries([], [], [], None)  # and set the actual symmetries
+
+        return reduced_qubit_ops
---
+++ test/mappers/second_quantization/test_jordan_wigner_mapper.py
@@ -0,0 +1,64 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test Jordan Wigner Mapper """
+
+import unittest
+
+from test import QiskitNatureTestCase
+
+from qiskit.opflow import X, Y, Z, I
+
+from qiskit_nature.drivers import HDF5Driver
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper
+from qiskit_nature.problems.second_quantization.molecular import fermionic_op_builder
+
+
+class TestJordanWignerMapper(QiskitNatureTestCase):
+    """ Test Jordan Wigner Mapper """
+
+    REF_H2 = \
+        - 0.81054798160031430 * (I ^ I ^ I ^ I) \
+        - 0.22575349071287365 * (Z ^ I ^ I ^ I) \
+        + 0.17218393211855787 * (I ^ Z ^ I ^ I) \
+        + 0.12091263243164174 * (Z ^ Z ^ I ^ I) \
+        - 0.22575349071287362 * (I ^ I ^ Z ^ I) \
+        + 0.17464343053355980 * (Z ^ I ^ Z ^ I) \
+        + 0.16614543242281926 * (I ^ Z ^ Z ^ I) \
+        + 0.17218393211855818 * (I ^ I ^ I ^ Z) \
+        + 0.16614543242281926 * (Z ^ I ^ I ^ Z) \
+        + 0.16892753854646372 * (I ^ Z ^ I ^ Z) \
+        + 0.12091263243164174 * (I ^ I ^ Z ^ Z) \
+        + 0.04523279999117751 * (X ^ X ^ X ^ X) \
+        + 0.04523279999117751 * (Y ^ Y ^ X ^ X) \
+        + 0.04523279999117751 * (X ^ X ^ Y ^ Y) \
+        + 0.04523279999117751 * (Y ^ Y ^ Y ^ Y)
+
+    def test_mapping(self):
+        """ Test mapping to qubit operator """
+        driver = HDF5Driver(hdf5_input=self.get_resource_path('test_driver_hdf5.hdf5',
+                                                              'drivers/hdf5d'))
+        q_molecule = driver.run()
+        fermionic_op = fermionic_op_builder.build_fermionic_op(q_molecule)
+        jw_mapper = JordanWignerMapper()
+        qubit_op = jw_mapper.map(fermionic_op)
+
+        # Note: The PauliSumOp equals, as used in the test below, use the equals of the
+        #       SparsePauliOp which in turn uses np.allclose() to determine equality of
+        #       coeffs. So the reference operator above will be matched on that basis so
+        #       we don't need to worry about tiny precision changes for any reason.
+
+        self.assertEqual(qubit_op, TestJordanWignerMapper.REF_H2)
+
+
+if __name__ == '__main__':
+    unittest.main()
