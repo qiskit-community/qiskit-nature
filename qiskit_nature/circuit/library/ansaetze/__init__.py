f41dc1403cc3c401bd245077947000d46c1cfe57
Max Rossmannek <oss@zurich.ibm.com>
2021-03-11 09:44:20 +0100

    Remove AdaptiveAnsatz interface
    Rather than duplicating the adaptive nature of an Ansatz through the
    introduction an additional interface we can rely on an adaptive
    algorithm to handle the list of excitations. Currently (AdaptVQE) does
    this anyways and the `push` and `pop` methods were merely utilities.
    Now, the BlueprintCircuit can simply have its operators replaced which
    will automatically invalidate a cached circuit and will cause rebuilding
    of all relevant information.

--- qiskit_nature/circuit/library/ansaetze/__init__.py
+++ qiskit_nature/circuit/library/ansaetze/__init__.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2020, 2021.
+# (C) Copyright IBM 2021.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -10,36 +10,13 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""
-===================================================================
-Chemistry Circuit Library (:mod:`qiskit_nature.circuit.library`)
-===================================================================
+"""Ansatz circuits and utilities thereof."""
 
-A collection of circuits used as building blocks or inputs of algorithms in chemistry.
-
-.. currentmodule:: qiskit_nature.circuit.library
-
-Initial states
-==============
-
-.. autosummary::
-   :toctree: ../stubs/
-   :nosignatures:
-
-   HartreeFock
-   VSCF
-
-"""
-
-from .adaptive_ansatz import AdaptiveAnsatz
-from .adapt_ucc import AdaptUCC
 from .evolved_operator_ansatz import EvolvedOperatorAnsatz
 from .excitation_builder import ExcitationBuilder
 from .ucc import UCC
 
 __all__ = [
-    'AdaptiveAnsatz',
-    'AdaptUCC',
     'EvolvedOperatorAnsatz',
     'ExcitationBuilder',
     'UCC',
