fb39c3bda4fd66282e6c3ce933babffee49afedd
dlasecki <DaRiO#94>
2021-03-19 13:08:31 +0100

    lint fix

--- qiskit_nature/__init__.py
+++ qiskit_nature/__init__.py
@@ -154,6 +154,7 @@ Submodules
 
 """
 
+from qiskit_nature.drivers.watson_hamiltonian import WatsonHamiltonian
 from .bosonic_operator import BosonicOperator
 from .exceptions import QiskitNatureError
 from .fermionic_operator import FermionicOperator
