d244b8671735b9d3f6c4771bbdd8e24f2a8f2b07
Max Rossmannek <oss@zurich.ibm.com>
2021-03-22 16:41:05 +0100

    Fix after merge

--- qiskit_nature/__init__.py
+++ qiskit_nature/__init__.py
@@ -137,7 +137,6 @@ Chemistry Classes
 
    BosonicOperator
    FermionicOperator
-   WatsonHamiltonian
 
 Submodules
 ==========
@@ -157,11 +156,9 @@ Submodules
 from .bosonic_operator import BosonicOperator
 from .exceptions import QiskitNatureError
 from .fermionic_operator import FermionicOperator
-from .watson_hamiltonian import WatsonHamiltonian
 
 __all__ = [
     'QiskitNatureError',
-    'WatsonHamiltonian',
     'BosonicOperator',
     'FermionicOperator',
 ]
