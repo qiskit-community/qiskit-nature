7c21a07729b3cb8b280b01d9126121760d5e3f9c
Max Rossmannek <oss@zurich.ibm.com>
2021-03-25 16:47:05 +0100

    Make basis configurable in VibrationalOpBuilder
    Although the argument is currently being ignored (and it is logged when
    this happens), having it already available makes the API ready for the
    addition in the future.

--- qiskit_nature/problems/second_quantization/vibrational/vibrational_op_builder.py
+++ qiskit_nature/problems/second_quantization/vibrational/vibrational_op_builder.py
@@ -10,36 +10,50 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 """ Vibrational operator builder. """
-from typing import Union, List
+from typing import Union, List, Optional
+
+import logging
 
 from qiskit_nature.drivers import WatsonHamiltonian
-from qiskit_nature.drivers.bosonic_bases import HarmonicBasis
+from qiskit_nature.drivers.bosonic_bases import BosonicBasis, HarmonicBasis
 
 from qiskit_nature.operators.second_quantization.vibrational_op import VibrationalOp
 from qiskit_nature.problems.second_quantization.vibrational.vibrational_label_builder import \
     _create_labels
 
 
+logger = logging.getLogger(__name__)
+
+
 def build_vibrational_op(watson_hamiltonian: WatsonHamiltonian,
                          num_modals: Union[int, List[int]],
-                         truncation_order: int) -> VibrationalOp:
+                         truncation_order: int,
+                         basis: Optional[BosonicBasis] = None,
+                         ) -> VibrationalOp:
     """
-    Builds a VibrationalOp based on a WatsonHamiltonian object.
+    Builds a :class:`VibrationalOp` based on a :class:`WatsonHamiltonian` object.
 
     Args:
-        watson_hamiltonian (WatsonHamiltonian): WatsonHamiltonian instance.
+        watson_hamiltonian: :class:`WatsonHamiltonian` instance.
         num_modals: the number of modals per mode.
         truncation_order: order at which an n-body expansion is truncated
+        basis: the :class:`BosonicBasis` to which the :class:`WatsonHamiltonian` gets converted.
+        Currently, this argument will be ignored until more bases are available. Therefore, it will
+        always use the :class:`HarmonicBasis` internally.
 
     Returns:
         VibrationalOp: VibrationalOp built from a WatsonHamiltonian object.
     """
+    if basis is not None:
+        logger.warning(
+            'The only supported `BosonicBasis` is the `HarmonicBasis`. However you specified '
+            '%s as an input, which will be ignored.', str(basis))
+
     num_modes = watson_hamiltonian.num_modes
 
     if isinstance(num_modals, int):
         num_modals = [num_modals] * num_modes
 
-    # TODO: make HarmonicBasis an argument and support other bases when implemented
     boson_hamilt_harm_basis = HarmonicBasis(watson_hamiltonian,
                                             num_modals, truncation_order).convert()
 
