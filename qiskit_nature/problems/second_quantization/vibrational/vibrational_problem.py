c70e24314ced39e7d632ddaaf1e7fe05e12ebe1f
Max Rossmannek <oss@zurich.ibm.com>
2021-03-25 17:38:52 +0100

    VibrationalOp and DirectMapper (#98)
    * WIP: DirectMapper
    
    * WIP: VibrationOp
    
    * Update VibrationOp tests
    
    * WIP: DirectMapper
    
    Milestone: mapped PauliSumOp matches the legacy one produced by
    BosonicOperator! :tada:
    
    * Unittest DirectMapper
    
    * Some refactoring
    
    * Log warning when particle number is not conserved
    
    * VibrationalProblem refactoring
    
    * Align all previous refactorings
    
    * Fix mypy
    
    * Fix style
    
    * Unify naming of basis_size and num_modals
    
    * Improve docs
    
    * Fix html
    
    * Fix spell
    
    * Remove fermionic anti-commutation
    
    * Fix spelling based on review
    
    Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
    
    * Apply updates based on review
    
    * Migrate VibrationalOp utils into class itself
    
    * Make basis configurable in VibrationalOpBuilder
    
    Although the argument is currently being ignored (and it is logged when
    this happens), having it already available makes the API ready for the
    addition in the future.
    
    Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>

--- qiskit_nature/problems/second_quantization/vibrational/vibrational_problem.py
+++ qiskit_nature/problems/second_quantization/vibrational/vibrational_problem.py
@@ -15,25 +15,25 @@ from typing import List, Optional, Union
 from qiskit_nature.drivers import BosonicDriver
 from qiskit_nature.operators.second_quantization import SecondQuantizedOp
 from qiskit_nature.problems.second_quantization.base_problem import BaseProblem
-from qiskit_nature.problems.second_quantization.vibrational.vibrational_spin_op_builder import \
-    build_vibrational_spin_op
+from qiskit_nature.problems.second_quantization.vibrational.vibrational_op_builder import \
+    build_vibrational_op
 from qiskit_nature.transformers import BaseTransformer
 
 
 class VibrationalProblem(BaseProblem):
     """Vibrational Problem"""
 
-    def __init__(self, bosonic_driver: BosonicDriver, basis_size: Union[int, List[int]],
+    def __init__(self, bosonic_driver: BosonicDriver, num_modals: Union[int, List[int]],
                  truncation_order: int, transformers: Optional[List[BaseTransformer]] = None):
         """
         Args:
             bosonic_driver: A bosonic driver encoding the molecule information.
             transformers: A list of transformations to be applied to the molecule.
-            basis_size: size of a basis
+            num_modals: the number of modals per mode.
             truncation_order: order at which an n-body expansion is truncated
         """
         super().__init__(bosonic_driver, transformers)
-        self.basis_size = basis_size
+        self.num_modals = num_modals
         self.truncation_order = truncation_order
 
     def second_q_ops(self) -> List[SecondQuantizedOp]:
@@ -45,9 +45,9 @@ class VibrationalProblem(BaseProblem):
         """
         watson_hamiltonian = self.driver.run()
         watson_hamiltonian_transformed = self._transform(watson_hamiltonian)
-        vibrational_spin_op = build_vibrational_spin_op(watson_hamiltonian_transformed,
-                                                        self.basis_size,
-                                                        self.truncation_order)
+        vibrational_spin_op = build_vibrational_op(watson_hamiltonian_transformed,
+                                                   self.num_modals,
+                                                   self.truncation_order)
 
         second_quantized_ops_list = [vibrational_spin_op]
 
