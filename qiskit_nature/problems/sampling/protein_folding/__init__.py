1b86fda0d8a802cdda45ecc74017a4737cbfa36c
Dariusz Lasecki <dal@zurich.ibm.com>
2021-06-22 16:16:17 +0200

    Docstrings extended.

--- qiskit_nature/problems/sampling/protein_folding/__init__.py
+++ qiskit_nature/problems/sampling/protein_folding/__init__.py
@@ -13,6 +13,15 @@
 Protein Folding Problems (:mod:`qiskit_nature.problems.sampling.protein_folding`)
 =================================================================================
 
+The protein to be folded is defined in a Peptide class. Each peptide consists of one and only one
+main chain and optionally several side chains. Side chains cannot be attached to first, second or
+last main beads. Each chain consists of beads that encode information about the turn that follows
+to another main bead (in case of main beads) or into a side bead (in case of side beads).
+Moreover, each bead is characterized by a letter which encodes its residue sequence that defines
+energy of interactions with other beads (unless interactions are random). Each side chain is
+attached to one and only one main bead. Currently, only side chains of length 1 (i.e. with 1
+bead) are supported. For more details consult the paper https://arxiv.org/pdf/1908.02163.pdf.
+
 .. currentmodule:: qiskit_nature.problems.sampling.protein_folding
 """
 
