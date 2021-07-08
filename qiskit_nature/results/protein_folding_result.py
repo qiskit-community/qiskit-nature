efb3b5514d3104ee2eb42388fdedd3a5f94bb71e
Dariusz Lasecki <dal@zurich.ibm.com>
2021-07-08 14:26:16 +0200

    protein_folding_result.py implemented

---
+++ qiskit_nature/results/protein_folding_result.py
@@ -0,0 +1,57 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""The protein folding result."""
+from typing import Union
+from qiskit.opflow import PauliOp
+from qiskit_nature.results import EigenstateResult
+from qiskit_nature.problems.sampling import ProteinFoldingProblem
+
+
+class ProteinFoldingResult(EigenstateResult):
+    """The protein folding result."""
+
+    def __init__(
+        self,
+        protein_folding_problem: ProteinFoldingProblem,
+        best_sequence: Union[str, PauliOp],
+    ) -> None:
+        super().__init__()
+        self._protein_folding_problem = protein_folding_problem
+        self._best_sequence: str = best_sequence
+
+    @property
+    def protein_folding_problem(self):
+        """Returns the protein folding probelm."""
+        return self._protein_folding_problem
+
+    @property
+    def best_sequence(self):
+        """Returns the best sequence."""
+        return self._best_sequence
+
+    def get_result_binary_vector(self) -> str:
+        """The ProteinFoldingProblem uses a compressed optimization problem that do not match the
+        number of qubits in the original objective function. This method calculates the original
+        version of the solution vector. Bits that can take any value without changing the
+        solution are denoted by '*'."""
+        unused_qubits = self._protein_folding_problem.unused_qubits
+        result = []
+        offset = 0
+        size = len(self._best_sequence)
+        for i in range(size):
+            index = size - 1 - i
+            while i + offset in unused_qubits:
+                result.append("*")
+                offset += 1
+            result.append(self._best_sequence[index])
+
+        return "".join(result[::-1])
---
+++ test/results/test_protein_folding_result.py
@@ -0,0 +1,85 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Tests ProteinFoldingResult."""
+from test import QiskitNatureTestCase
+from qiskit_nature.results.protein_folding_result import ProteinFoldingResult
+from qiskit_nature.problems.sampling.protein_folding.interactions.miyazawa_jernigan_interaction import (
+    MiyazawaJerniganInteraction,
+)
+from qiskit_nature.problems.sampling.protein_folding.peptide.peptide import Peptide
+from qiskit_nature.problems.sampling.protein_folding.protein_folding_problem import (
+    ProteinFoldingProblem,
+)
+from qiskit_nature.problems.sampling.protein_folding.penalty_parameters import PenaltyParameters
+
+
+class TestProteinFoldingProblem(QiskitNatureTestCase):
+    """Tests ProteinFoldingResult."""
+
+    def test_get_result_binary_vector(self):
+        """Tests if a protein folding result returns a correct expanded best sequence if not
+        qubits compressed."""
+        lambda_back = 10
+        lambda_chiral = 10
+        lambda_1 = 10
+        lambda_contacts = 10
+        penalty_terms = PenaltyParameters(lambda_chiral, lambda_back, lambda_1, lambda_contacts)
+
+        main_chain_residue_seq = ["S", "A", "A", "S", "S", "A", "S", "A", "A"]
+        main_chain_len = 9
+        side_chain_lens = [0, 0, 1, 1, 1, 1, 1, 1, 0]
+        side_chain_residue_sequences = [None, None, "A", "A", "A", "A", "A", "A", None]
+
+        peptide = Peptide(
+            main_chain_len, main_chain_residue_seq, side_chain_lens, side_chain_residue_sequences
+        )
+
+        mj_interaction = MiyazawaJerniganInteraction()
+
+        protein_folding_problem = ProteinFoldingProblem(peptide, mj_interaction, penalty_terms)
+        best_sequence = "101110010"
+
+        protein_folding_result = ProteinFoldingResult(protein_folding_problem, best_sequence)
+
+        result = protein_folding_result.get_result_binary_vector()
+
+        self.assertEqual(result, best_sequence)
+
+    def test_get_result_binary_vector_compressed(self):
+        """Tests if a protein folding result returns a correct expanded best sequence."""
+        lambda_back = 10
+        lambda_chiral = 10
+        lambda_1 = 10
+        lambda_contacts = 10
+        penalty_terms = PenaltyParameters(lambda_chiral, lambda_back, lambda_1, lambda_contacts)
+
+        main_chain_residue_seq = ["S", "A", "A", "S", "S", "A", "S", "A", "A"]
+        main_chain_len = 9
+        side_chain_lens = [0, 0, 1, 1, 1, 1, 1, 1, 0]
+        side_chain_residue_sequences = [None, None, "A", "A", "A", "A", "A", "A", None]
+
+        peptide = Peptide(
+            main_chain_len, main_chain_residue_seq, side_chain_lens, side_chain_residue_sequences
+        )
+
+        mj_interaction = MiyazawaJerniganInteraction()
+
+        protein_folding_problem = ProteinFoldingProblem(peptide, mj_interaction, penalty_terms)
+        best_sequence = "101110010"
+        protein_folding_problem._unused_qubits = [0, 1, 2, 5, 7]
+
+        protein_folding_result = ProteinFoldingResult(protein_folding_problem, best_sequence)
+
+        result = protein_folding_result.get_result_binary_vector()
+        expected_sequence = "101110*0*10***"
+
+        self.assertEqual(result, expected_sequence)
