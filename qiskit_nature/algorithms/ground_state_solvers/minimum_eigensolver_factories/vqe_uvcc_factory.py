5154e02717e01aa7f6fd30e55518ef4281430b6d
Max Rossmannek <oss@zurich.ibm.com>
2021-09-28 18:03:55 +0200

    Fix #375

--- qiskit_nature/algorithms/ground_state_solvers/minimum_eigensolver_factories/vqe_uvcc_factory.py
+++ qiskit_nature/algorithms/ground_state_solvers/minimum_eigensolver_factories/vqe_uvcc_factory.py
@@ -46,6 +46,8 @@ class VQEUVCCFactory(MinimumEigensolverFactory):
         include_custom: bool = False,
         ansatz: Optional[UVCC] = None,
         initial_state: Optional[QuantumCircuit] = None,
+        callback: Optional[Callable[[int, np.ndarray, float, float], None]] = None,
+        **kwargs,
     ) -> None:
         """
         Args:
@@ -72,6 +74,12 @@ class VQEUVCCFactory(MinimumEigensolverFactory):
             initial_state: Allows specification of a custom `QuantumCircuit` to be used as the
                 initial state of the ansatz. If this is never set by the user, the factory will
                 default to the :class:`~.VSCF` state.
+            callback: a callback that can access the intermediate data during the optimization.
+                Four parameter values are passed to the callback as follows during each evaluation
+                by the optimizer for its current set of parameters as it works towards the minimum.
+                These are: the evaluation count, the optimizer parameters for the
+                ansatz, the evaluated mean and the evaluated standard deviation.`
+            kwargs: any additional keyword arguments will be passed on to the VQE.
         """
         self._quantum_instance = quantum_instance
         self._optimizer = optimizer
@@ -81,6 +89,7 @@ class VQEUVCCFactory(MinimumEigensolverFactory):
         self._include_custom = include_custom
         self.ansatz = ansatz
         self.initial_state = initial_state
+        self.callback = callback
         self._vqe = VQE(
             ansatz=None,
             quantum_instance=self._quantum_instance,
@@ -89,6 +98,8 @@ class VQEUVCCFactory(MinimumEigensolverFactory):
             gradient=self._gradient,
             expectation=self._expectation,
             include_custom=self._include_custom,
+            callback=self._callback,
+            **kwargs,
         )
 
     @property
@@ -172,6 +183,16 @@ class VQEUVCCFactory(MinimumEigensolverFactory):
         """Setter of the initial state. If ``None`` is passed, this factory will default to using
         the :class:`~.VSCF`."""
         self._initial_state = initial_state
+
+    @property
+    def callback(self) -> Optional[Callable[[int, np.ndarray, float, float], None]]:
+        """Returns the callback."""
+        return self._callback
+
+    @callback.setter
+    def callback(self, callback: Optional[Callable[[int, np.ndarray, float, float], None]]) -> None:
+        """Sets the callback."""
+        self._callback = callback
 
     def get_solver(  # type: ignore[override]
         self,
