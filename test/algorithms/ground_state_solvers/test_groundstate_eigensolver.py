49870c4d62ced09692ba8a4bf80f33097c9bcf96
Max Rossmannek <oss@zurich.ibm.com>
2021-11-08 12:15:40 +0100

    Run black

--- test/algorithms/ground_state_solvers/test_groundstate_eigensolver.py
+++ test/algorithms/ground_state_solvers/test_groundstate_eigensolver.py
@@ -149,21 +149,45 @@ class TestGroundStateEigensolver(QiskitNatureTestCase):
             calc = GroundStateEigensolver(self.qubit_converter, solver)
             res = calc.solve(self.electronic_structure_problem)
             self.assertAlmostEqual(res.total_energies[0], self.reference_energy, places=6)
-            self.assertTrue(np.all(isinstance(aux_op, dict) for aux_op in res.aux_operator_eigenvalues))
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["ParticleNumber"][0], 2.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["ParticleNumber"][1], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["AngularMomentum"][0], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["AngularMomentum"][1], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["Magnetization"][0], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["Magnetization"][1], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["DipoleMomentX"][0], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["DipoleMomentX"][1], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["DipoleMomentY"][0], 0.0, places=6)
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["DipoleMomentY"][1], 0.0, places=6)
+            self.assertTrue(
+                np.all(isinstance(aux_op, dict) for aux_op in res.aux_operator_eigenvalues)
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["ParticleNumber"][0], 2.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["ParticleNumber"][1], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["AngularMomentum"][0], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["AngularMomentum"][1], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["Magnetization"][0], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["Magnetization"][1], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["DipoleMomentX"][0], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["DipoleMomentX"][1], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["DipoleMomentY"][0], 0.0, places=6
+            )
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["DipoleMomentY"][1], 0.0, places=6
+            )
             self.assertAlmostEqual(
                 res.aux_operator_eigenvalues[0]["DipoleMomentZ"][0], -1.3889487, places=6
             )
-            self.assertAlmostEqual(res.aux_operator_eigenvalues[0]["DipoleMomentZ"][1], 0.0, places=6)
+            self.assertAlmostEqual(
+                res.aux_operator_eigenvalues[0]["DipoleMomentZ"][1], 0.0, places=6
+            )
         finally:
             settings.dict_aux_operators = False
 
