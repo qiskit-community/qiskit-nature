Fix the units of the Molecule geometry
PySCF's method `atom_coords()` defaults to Bohr although the rest of the
module defaults to Angstrom. This went unnoticed and is now fixed.

test/drivers/second_quantization/test_driver.py
@@ -149,7 +149,7 @@ class TestDriver(ABC):
             coords = [coord for _, coord in molecule.geometry]
             self.log.debug("atom xyz %s", coords)
             np.testing.assert_array_almost_equal(
-                coords, [[0.0, 0.0, 0.0], [0.0, 0.0, 1.3889]], decimal=4
+                coords, [[0.0, 0.0, 0.0], [0.0, 0.0, 0.735]], decimal=4
             )
 
     def test_driver_result_basis_transform(self):