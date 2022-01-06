Fix occupation computation of non-singlet states (backport #475) (#494)
* Fix occupation computation of non-singlet states (#475)

Prior to this fix, the `ParticleNumber` property would incorrectly
compute the alpha and beta occupations:
```python
    prop = ParticleNumber(4, (2, 1), [2.0, 1.0])
    # >>> [1.0, 0.5]
    print(prop.occupation_alpha)
    # >>> [1.0, 0.5]
    print(prop.occupation_beta)
```

After this fix, the computation is corrected to return:
```python
    prop = ParticleNumber(4, (2, 1), [2.0, 1.0])
    # >>> [1.0, 1.0]
    print(prop.occupation_alpha)
    # >>> [1.0, 0.0]
    print(prop.occupation_beta)
```

Co-authored-by: Manoel Marques <Manoel.Marques@ibm.com>
(cherry picked from commit 7dc4524127598082cd40f7a2d6a752b7f8ed9213)

* Fix copyright

* Fix copyright

Co-authored-by: Max Rossmannek <oss@zurich.ibm.com>

qiskit_nature/properties/second_quantization/electronic/particle_number.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021.
+# (C) Copyright IBM 2021, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -78,8 +78,8 @@ class ParticleNumber(ElectronicProperty):
             self._occupation_beta = [1.0 for _ in range(self._num_beta)]
             self._occupation_beta += [0.0] * (num_spin_orbitals // 2 - len(self._occupation_beta))
         elif occupation_beta is None:
-            self._occupation_alpha = [o / 2.0 for o in occupation]
-            self._occupation_beta = [o / 2.0 for o in occupation]
+            self._occupation_alpha = [np.ceil(o / 2) for o in occupation]
+            self._occupation_beta = [np.floor(o / 2) for o in occupation]
         else:
             self._occupation_alpha = occupation  # type: ignore
             self._occupation_beta = occupation_beta  # type: ignore