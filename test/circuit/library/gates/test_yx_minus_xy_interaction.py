fcb15b034c069dd5bfaa7ac46a2ef4ee79349388
Kevin J. Sung <kevjsung@umich.edu>
2022-01-07 09:33:14 -0500

    use ddt

--- test/circuit/library/gates/test_yx_minus_xy_interaction.py
+++ test/circuit/library/gates/test_yx_minus_xy_interaction.py
@@ -14,36 +14,38 @@
 
 from test import QiskitNatureTestCase
 import numpy as np
+from ddt import data, ddt, unpack
 
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Operator
 from qiskit_nature.circuit.library import YXMinusXYInteractionGate
 
 
+@ddt
 class TestYXMinusXYInteractionGate(QiskitNatureTestCase):
     """Tests for YXMinusXYInteractionGate gate"""
 
-    def test_matrix(self):
+    @unpack
+    @data(
+        (0, np.eye(4)),
+        (
+            np.pi / 4,
+            np.array(
+                [
+                    [1, 0, 0, 0],
+                    [0, np.sqrt(2) / 2, np.sqrt(2) / 2, 0],
+                    [0, -np.sqrt(2) / 2, np.sqrt(2) / 2, 0],
+                    [0, 0, 0, 1],
+                ]
+            ),
+        ),
+        (np.pi / 2, np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0], [0, 0, 0, 1]])),
+        (np.pi, np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])),
+        (2 * np.pi, np.eye(4)),
+    )
+    def test_matrix(self, theta: float, expected: np.ndarray):
         """Test matrix."""
-        gate = YXMinusXYInteractionGate(0)
-        expected = np.eye(4)
-        np.testing.assert_allclose(gate.to_matrix(), expected, atol=1e-7)
-
-        gate = YXMinusXYInteractionGate(np.pi / 4)
-        a = np.sqrt(2) / 2
-        expected = np.array([[1, 0, 0, 0], [0, a, a, 0], [0, -a, a, 0], [0, 0, 0, 1]])
-        np.testing.assert_allclose(gate.to_matrix(), expected, atol=1e-7)
-
-        gate = YXMinusXYInteractionGate(np.pi / 2)
-        expected = np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
-        np.testing.assert_allclose(gate.to_matrix(), expected, atol=1e-7)
-
-        gate = YXMinusXYInteractionGate(np.pi)
-        expected = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        np.testing.assert_allclose(gate.to_matrix(), expected, atol=1e-7)
-
-        gate = YXMinusXYInteractionGate(2 * np.pi)
-        expected = np.eye(4)
+        gate = YXMinusXYInteractionGate(theta)
         np.testing.assert_allclose(gate.to_matrix(), expected, atol=1e-7)
 
     def test_inverse(self):
