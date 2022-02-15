8a6b50f59d345f55f6194163694873da77048298
Kevin J. Sung <kevjsung@umich.edu>
2022-02-15 10:17:40 -0500

    document iteration in class docstring

--- qiskit_nature/operators/second_quantization/fermionic_op.py
+++ qiskit_nature/operators/second_quantization/fermionic_op.py
@@ -207,6 +207,12 @@ class FermionicOp(SecondQuantizedOp):
             some processing
             fermion += FermionicOp(somedata)
 
+    **Iteration**
+
+    FermionicOps are iterable. Iterating a FermionicOp yields (term, coefficient) pairs
+    describing the terms contained in the operator. Each term is a list of tuples of the
+    form (action, index), where the action is either "+" or "-" and the index is the integer
+    index of the factor in the term.
     """
     # Warn only once
     _display_format_warn = True
