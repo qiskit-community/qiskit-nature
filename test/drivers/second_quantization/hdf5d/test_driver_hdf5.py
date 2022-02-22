937cf0e71e033cf3fa7105564bdde80a78c424d3
Max Rossmannek <oss@zurich.ibm.com>
2022-02-22 12:52:55 +0100

    HDF5 Driver Update (#550)
    * refactor: basic HDF5Driver update
    
    * test: test both HDF5 type supports
    
    * Fix copyright
    
    * test: update new expected HDF5 file
    
    * ref: align new HDF5Driver with #554
    
    * feat: add HDF5Driver.convert utility method
    
    * Add reno
    
    * Fix linters
    
    * refactor: do not replace by default
    
    * refactor: use `_new.hdf5` instead of `.hdf5.new`
    
    * refactor: update wording
    
    * fix: Python <3.9 compatibility
    
    * Update docstring
    
    Co-authored-by: Panagiotis Barkoutsos <bpa@zurich.ibm.com>

--- test/drivers/second_quantization/hdf5d/test_driver_hdf5.py
+++ test/drivers/second_quantization/hdf5d/test_driver_hdf5.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2018, 2021.
+# (C) Copyright IBM 2018, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -12,7 +12,12 @@
 
 """ Test Driver HDF5 """
 
+import os
+import pathlib
+import shutil
+import tempfile
 import unittest
+
 from test import QiskitNatureTestCase
 from test.drivers.second_quantization.test_driver import TestDriver
 from qiskit_nature.drivers.second_quantization import HDF5Driver
@@ -26,6 +31,72 @@ class TestDriverHDF5(QiskitNatureTestCase, TestDriver):
         driver = HDF5Driver(
             hdf5_input=self.get_resource_path(
                 "test_driver_hdf5.hdf5", "drivers/second_quantization/hdf5d"
+            )
+        )
+        self.driver_result = driver.run()
+
+    def test_convert(self):
+        """Test the legacy-conversion method."""
+        legacy_file_path = self.get_resource_path(
+            "test_driver_hdf5_legacy.hdf5", "drivers/second_quantization/hdf5d"
+        )
+
+        with self.subTest("replace=True"):
+            # pylint: disable=consider-using-with
+            tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".hdf5")
+            tmp_file.close()
+            os.unlink(tmp_file.name)
+            shutil.copy(legacy_file_path, tmp_file.name)
+            try:
+                driver = HDF5Driver(tmp_file.name)
+                driver.convert(replace=True)
+                with self.assertRaises(AssertionError):
+                    # NOTE: we can use assertNoLogs once Python 3.10 is the default
+                    with self.assertLogs(
+                        logger="qiskit_nature.drivers.second_quantization.hdf5d.hdf5driver",
+                        level="WARNING",
+                    ):
+                        driver.run()
+            finally:
+                os.unlink(tmp_file.name)
+
+        with self.subTest("replace=False"):
+            # pylint: disable=consider-using-with
+            tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".hdf5")
+            tmp_file.close()
+            new_file_name = pathlib.Path(tmp_file.name).with_name(
+                str(pathlib.Path(tmp_file.name).stem) + "_new.hdf5"
+            )
+            os.unlink(tmp_file.name)
+            shutil.copy(legacy_file_path, tmp_file.name)
+            try:
+                driver = HDF5Driver(tmp_file.name)
+                driver.convert(replace=False)
+                with self.assertLogs(
+                    logger="qiskit_nature.drivers.second_quantization.hdf5d.hdf5driver",
+                    level="WARNING",
+                ):
+                    driver.run()
+                with self.assertRaises(AssertionError):
+                    # NOTE: we can use assertNoLogs once Python 3.10 is the default
+                    with self.assertLogs(
+                        logger="qiskit_nature.drivers.second_quantization.hdf5d.hdf5driver",
+                        level="WARNING",
+                    ):
+                        HDF5Driver(new_file_name).run()
+            finally:
+                os.unlink(tmp_file.name)
+                os.unlink(new_file_name)
+
+
+class TestDriverHDF5Legacy(QiskitNatureTestCase, TestDriver):
+    """HDF5 Driver legacy file-support tests."""
+
+    def setUp(self):
+        super().setUp()
+        driver = HDF5Driver(
+            hdf5_input=self.get_resource_path(
+                "test_driver_hdf5_legacy.hdf5", "drivers/second_quantization/hdf5d"
             )
         )
         self.driver_result = driver.run()
