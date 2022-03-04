9a0624202915603ff7c6369ad6df18b455e4bf64
Abby Mitchell <abby.s.mitchell@gmail.com>
2022-03-04 17:33:24 -0500

    Reformatted spacing for code example

--- qiskit_nature/circuit/library/ansatzes/utils/fermionic_excitation_generator.py
+++ qiskit_nature/circuit/library/ansatzes/utils/fermionic_excitation_generator.py
@@ -93,6 +93,7 @@ def generate_fermionic_excitations(
     generalized: bool = False,
     preserve_spin: bool = True,
 ) -> List[Tuple[Tuple[int, ...], Tuple[int, ...]]]:
+    # pylint: disable=line-too-long
     """Generates all possible excitations with the given number of excitations for the specified
     number of particles distributed among the given number of spin orbitals.
 
@@ -128,10 +129,8 @@ def generate_fermionic_excitations(
     Examples:
         Generate excitations with basic inputs.
 
-        >>> from qiskit_nature.circuit.library.ansatzes.utils.fermionic_excitation_generator import (\
-        generate_fermionic_excitations)
-        >>> generate_fermionic_excitations(num_excitations=1, num_spin_orbitals=6,\
-        num_particles=(1,1))
+        >>> from qiskit_nature.circuit.library.ansatzes.utils.fermionic_excitation_generator import generate_fermionic_excitations
+        >>> generate_fermionic_excitations(num_excitations=1, num_spin_orbitals=6, num_particles=(1,1))
         [((0,), (1,)), ((0,), (2,)), ((3,), (4,)), ((3,), (5,))]
 
         Generate generalized excitations.
