6f634251ccfa0ee21eda5502e37c53a30d661c09
Declan Millar <declan.millar@ibm.com>
2022-03-09 14:54:17 +0000

    improve abstract base Initializer class

--- qiskit_nature/initializers/initializer.py
+++ qiskit_nature/initializers/initializer.py
@@ -12,29 +12,28 @@
 
 """ Base Initializer class """
 
-import abc
+from abc import ABC, abstractmethod
 from typing import Sequence, Tuple
 
 import numpy as np
 
 
-class Initializer(abc.ABC):
-    """Base Initializer class."""
+class Initializer(ABC):
+    """Abstract base Initializer class."""
 
     def __init__(self) -> None:
         super().__init__()
-        self._coefficients = None
-        self._energy_corrections = None
 
     @property
+    @abstractmethod
     def coefficients(self) -> np.ndarray:
-        """Get the coefficients for the molecule.
-
-        Returns:
-            The initializer coefficients.
         """
-        return self._coefficients
+        Returns:
+            The coefficients based on the Initializer.
+        """
+        raise NotImplementedError()
 
+    @abstractmethod
     def compute_corrections(
         self,
         excitations: Sequence,
@@ -42,9 +41,6 @@ class Initializer(abc.ABC):
         """Compute the coefficients for the molecule.
 
         Returns:
-            The initializer coefficients.
+            The initializer coefficients and energy corrections.
         """
-        coeffs = np.zeroes(len(excitations))
-        self._coefficients = coeffs
-        self._energy_corrections = coeffs
-        return coeffs, coeffs
+        raise NotImplementedError()
