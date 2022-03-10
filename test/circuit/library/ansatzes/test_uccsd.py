4fbda0153093cd3e31c2704f48b05f522b5cc89a
Declan Millar <declan.millar@ibm.com>
2022-03-10 15:26:17 +0000

    remove mp2 tests from uccsd

--- test/circuit/library/ansatzes/test_uccsd.py
+++ test/circuit/library/ansatzes/test_uccsd.py
@@ -16,13 +16,11 @@ from test import QiskitNatureTestCase
 from test.circuit.library.ansatzes.test_ucc import assert_ucc_like_ansatz
 
 from ddt import ddt, data, unpack
-import numpy as np
 
 from qiskit_nature.circuit.library import UCCSD
 from qiskit_nature.mappers.second_quantization import JordanWignerMapper
 from qiskit_nature.operators.second_quantization import FermionicOp
 from qiskit_nature.converters.second_quantization import QubitConverter
-from qiskit_nature.initializers import MP2Initializer
 
 
 @ddt
@@ -194,63 +192,3 @@ class TestUCCSD(QiskitNatureTestCase):
         )
 
         assert_ucc_like_ansatz(self, ansatz, num_spin_orbitals, expect)
-
-    @unpack
-    @data(
-        # No initializer
-        (8, (2, 2), None, np.zeros(26)),
-    )
-    def test_with_no_initializer(self, num_spin_orbitals, num_particles, initializer, expected):
-        """Test preferred_init_points is all zero with no initializer."""
-
-        converter = QubitConverter(JordanWignerMapper())
-
-        ansatz = UCCSD(
-            qubit_converter=converter,
-            num_particles=num_particles,
-            num_spin_orbitals=num_spin_orbitals,
-            initializer=initializer,
-        )
-        ansatz._build()
-        np.testing.assert_array_equal(ansatz.preferred_init_points, expected)
-
-    @unpack
-    @data(
-        # MP2Initializer h2 molecule
-        (
-            4,
-            (1, 1),
-            np.asarray([-0.595463473004833, 0.714165284454473]),
-            np.asarray(
-                [
-                    [
-                        [[0.6823895331520421, 0.0], [0.0, 0.6707327783087585]],
-                        [[0.0, 0.17900057606140662], [0.17900057606140676, 0.0]],
-                    ],
-                    [
-                        [[0.0, 0.17900057606140674], [0.17900057606140682, 0.0]],
-                        [[0.6707327783087589, 0.0], [0.0, 0.7051056321727855]],
-                    ],
-                ],
-            ),
-            np.asarray([0.0, 0.0, -0.06834019757197064]),
-        )
-    )
-    def test_with_mp2_initializer(
-        self, num_spin_orbitals, num_particles, orbital_energies, integral_matrix, expected
-    ):
-        """Test preferred_init_points with MP2 initializer."""
-
-        initializer = MP2Initializer(num_spin_orbitals, orbital_energies, integral_matrix)
-
-        converter = QubitConverter(JordanWignerMapper())
-
-        ansatz = UCCSD(
-            qubit_converter=converter,
-            num_particles=num_particles,
-            num_spin_orbitals=num_spin_orbitals,
-            initializer=initializer,
-        )
-        ansatz._build()
-
-        np.testing.assert_array_almost_equal(ansatz.preferred_init_points, expected)
