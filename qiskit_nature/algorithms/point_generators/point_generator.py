c05a91d7b7890e003d69ec66594dac531aee582d
Declan Millar <declan.millar@ibm.com>
2022-03-11 10:45:44 +0000

    Add interface for point generators

---
+++ qiskit_nature/algorithms/point_generators/point_generator.py
@@ -0,0 +1,27 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""The initial point generator interface interface."""
+
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class PointGenerator(ABC):
+    """The initial point generator interface."""
+
+    @property
+    @abstractmethod
+    def initial_point(self) -> np.ndarray:
+        """Returns the initial point."""
+        raise NotImplementedError
