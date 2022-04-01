c6eec6e68d384e36c39bacb0369da1b1db54d3e2
Kevin J. Sung <kevjsung@umich.edu>
2022-04-01 02:40:20 -0400

    Define FermionicOp.terms (#544)
    * define __iter__ for FermionicOp
    
    * document iteration in class docstring
    
    * add release note
    
    * use terms method name instead of defining __iter__
    
    * import Iterator from collections.abc
    
    * fix test
    
    Co-authored-by: Panagiotis Barkoutsos <bpa@zurich.ibm.com>

--- test/operators/second_quantization/test_fermionic_op.py
+++ test/operators/second_quantization/test_fermionic_op.py
@@ -146,6 +146,16 @@ class TestFermionicOp(QiskitNatureTestCase):
         )
         self.assertListEqual(actual._data, desired)
 
+    def test_terms(self):
+        """Test terms"""
+        op = FermionicOp([("+", 1.0), ("N", 2.0), ("-+", 3.0)], display_format="dense")
+        expected = {
+            ((("+", 0),), 1.0),
+            ((("+", 0), ("-", 0)), 2.0),
+            ((("-", 0), ("+", 1)), 3.0),
+        }
+        self.assertEqual(set(op.terms()), expected)
+
     def test_register_length(self):
         """Test inference of register_length"""
         op = FermionicOp([("+_1", 1.0), ("", 1.0)], display_format="dense")
