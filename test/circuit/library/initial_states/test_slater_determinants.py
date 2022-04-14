c01bc1e08c060930f95640d22496452b8a91f9d5
Kevin J. Sung <kevjsung@umich.edu>
2022-04-14 13:19:59 -0400

    add slater determinants test file

---
+++ test/circuit/library/initial_states/test_slater_determinants.py
@@ -0,0 +1,75 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test Slater determinant state preparation circuits."""
+
+from test import QiskitNatureTestCase
+
+import numpy as np
+from qiskit.quantum_info import random_hermitian, Statevector
+from qiskit_nature.circuit.library import SlaterDeterminant
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper
+from qiskit_nature.operators.second_quantization.quadratic_hamiltonian import QuadraticHamiltonian
+
+
+class TestSlaterDeterminant(QiskitNatureTestCase):
+    """Tests for preparing Slater determinants."""
+
+    def test_slater_determinant(self):
+        """Test preparing Slater determinants."""
+        n_orbitals = 5
+        converter = QubitConverter(JordanWignerMapper())
+        hermitian_part = random_hermitian(n_orbitals).data
+        constant = np.random.uniform(-10, 10)
+        quad_ham = QuadraticHamiltonian(hermitian_part, constant=constant)
+        (
+            transformation_matrix,
+            orbital_energies,
+            transformed_constant,
+        ) = quad_ham.diagonalizing_bogoliubov_transform()
+        fermionic_op = quad_ham.to_fermionic_op()
+        qubit_op = converter.convert(fermionic_op)
+        matrix = qubit_op.to_matrix()
+        for n_particles in range(n_orbitals + 1):
+            circuit = SlaterDeterminant(transformation_matrix[:n_particles], converter)
+            final_state = np.array(Statevector(circuit))
+            eig = np.sum(orbital_energies[:n_particles]) + transformed_constant
+            np.testing.assert_allclose(matrix @ final_state, eig * final_state, atol=1e-7)
+
+    def test_no_side_effects(self):
+        """Test that the routines don't mutate the input array."""
+        n_orbitals = 5
+        n_particles = 3
+        converter = QubitConverter(JordanWignerMapper())
+        hermitian_part = random_hermitian(n_orbitals).data
+        constant = np.random.uniform(-10, 10)
+
+        quad_ham = QuadraticHamiltonian(hermitian_part, constant=constant)
+        transformation_matrix, _, _ = quad_ham.diagonalizing_bogoliubov_transform()
+        original = transformation_matrix.copy()
+        _ = SlaterDeterminant(transformation_matrix[:n_particles], qubit_converter=converter)
+        np.testing.assert_allclose(transformation_matrix, original, atol=1e-7)
+
+    def test_validation(self):
+        """Test input validation."""
+        with self.assertRaisesRegex(ValueError, "2-dimensional"):
+            _ = SlaterDeterminant(np.ones((2, 2, 2)))
+        with self.assertRaisesRegex(ValueError, "orthonormal"):
+            _ = SlaterDeterminant(np.ones((3, 2)))
+        with self.assertRaisesRegex(ValueError, "orthonormal"):
+            _ = SlaterDeterminant(np.ones((2, 3)))
+
+    def test_circuit_kwargs(self):
+        """Test that circuit keyword arguments are actually passed through."""
+        circuit = SlaterDeterminant(np.eye(2), name="abcd")
+        assert circuit.name == "abcd"
