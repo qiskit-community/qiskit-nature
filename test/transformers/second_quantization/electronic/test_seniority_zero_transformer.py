540a2fb3aeb995431b5cfbd1903bd49c02bf8fe0
Julian Schuhmacher <jsc@zurich.ibm.com>
2022-04-22 10:18:28 +0200

    Add test for building the restricted second_q_ops.

--- test/transformers/second_quantization/electronic/test_seniority_zero_transformer.py
+++ test/transformers/second_quantization/electronic/test_seniority_zero_transformer.py
@@ -16,6 +16,7 @@ import unittest
 
 from test import QiskitNatureTestCase
 
+import json
 import numpy as np
 
 from qiskit_nature.drivers.second_quantization.hdf5d.hdf5driver import HDF5Driver
@@ -45,8 +46,22 @@ class TestSeniorityZeroTransformer(QiskitNatureTestCase):
                 electronic_energy_exp.get_electronic_integral(ElectronicBasis.MO, 2).get_matrix(),
             )
 
-    def test_transformation_to_restricted_operator(self):
-        """Test transformation to restricted operator."""
+    def assertOperatorsEqual(self, operator, expected_operator_file):
+        """Compare two operators, where the expected operator is given in a JSON file."""
+        with open(
+            self.get_resource_path(
+                expected_operator_file, "transformers/second_quantization/electronic"
+            ),
+            "r",
+            encoding="utf8",
+        ) as file:
+            expected = json.load(file)
+        for op, expected_op in zip(operator.to_list(), expected):
+            self.assertEqual(op[0], expected_op[0])
+            self.assertTrue(np.isclose(op[1], expected_op[1]))
+
+    def test_transformation_to_restricted_formalism(self):
+        """Test transformation to restricted formalism."""
         driver = HDF5Driver(
             hdf5_input=self.get_resource_path(
                 "LiH_sto3g.hdf5", "transformers/second_quantization/electronic"
@@ -64,6 +79,31 @@ class TestSeniorityZeroTransformer(QiskitNatureTestCase):
         ).run()
 
         self.assertDriverResult(driver_result_restricted, expected)
+
+    def test_restricted_second_q_ops(self):
+        """Test building of second quantization operators in restricted formalism."""
+        driver = HDF5Driver(
+            hdf5_input=self.get_resource_path(
+                "LiH_sto3g.hdf5", "transformers/second_quantization/electronic"
+            )
+        )
+        driver_result = driver.run()
+
+        transformer = SeniorityZeroTransformer()
+        driver_result_restricted = transformer.transform(driver_result)
+        second_q_ops = driver_result_restricted.second_q_ops()
+
+        with self.subTest("Electronic Energy"):
+            operator = second_q_ops["ElectronicEnergy"]
+            self.assertOperatorsEqual(
+                operator, expected_operator_file="LiH_sto3g_restricted_electronic_energy_op.json"
+            )
+
+        with self.subTest("Particle Number"):
+            operator = second_q_ops["ParticleNumber"]
+            self.assertOperatorsEqual(
+                operator, expected_operator_file="LiH_sto3g_restricted_particle_number_op.json"
+            )
 
 
 if __name__ == "__main__":
