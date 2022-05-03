1b0247b88483539168025e67689bfe204ff6399f
Marc Sanz Drudis <93275620+MarcDrudis@users.noreply.github.com>
2022-05-03 16:04:09 +0200

    Update qiskit_nature/results/protein_folding_tools/protein_xyz.py
    Co-authored-by: dlasecki <dal@zurich.ibm.com>

--- qiskit_nature/results/protein_folding_tools/protein_xyz.py
+++ qiskit_nature/results/protein_folding_tools/protein_xyz.py
@@ -9,8 +9,8 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-"""An auxiliary class that gets the coordinates of the aminoacids of a molecule
- in ProteinFoldingResult """
+"""An auxiliary class that gets the coordinates of aminoacids of a molecule
+ in ProteinFoldingResult."""
 from typing import Union, List, Optional
 import numpy as np
 
