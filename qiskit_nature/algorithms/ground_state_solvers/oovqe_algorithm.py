Fix initial point, refactor orbital rotation

qiskit_nature/algorithms/ground_state_solvers/oovqe_algorithm.py
@@ -53,7 +53,7 @@ class OrbitalOptimizationVQE(GroundStateEigensolver):
     def __init__(
         self,
         qubit_converter: QubitConverter,
-        solver: Union[MinimumEigensolver, MinimumEigensolverFactory],
+        solver: Union[MinimumEigensolver, MinimumEigensolverFactory] = None,
         initial_point: Union[ListOrDict, np.ndarray] = None,
     ) -> None:
         super().__init__(qubit_converter, solver)
@@ -68,6 +68,8 @@ class OrbitalOptimizationVQE(GroundStateEigensolver):
         self.initial_point = initial_point  #
         self.bounds_oo = None  # in the future: set by user
         self.bounds = None  # ansatz + oo
+
+        self.operator = None
 
     def set_initial_point(self, initial_pt_scalar: float = 1e-1) -> None:
         """Initializes the initial point for the algorithm if the user does not provide his own.
@@ -110,8 +112,9 @@ class OrbitalOptimizationVQE(GroundStateEigensolver):
         main_operator = self._qubit_converter.convert(
             main_second_q_op,
             num_particles=problem.num_particles,
-            sector_locator=problem.symmetry_sector_locator,
+            sector_locator=None,
         )
+        self.operator = main_operator
         aux_ops = self._qubit_converter.convert_match(aux_second_q_ops)
 
         if aux_operators is not None:
@@ -144,24 +147,27 @@ class OrbitalOptimizationVQE(GroundStateEigensolver):
         """Doctstring"""
         problem = copy.copy(self.problem)
         grouped_property_transformed = problem.grouped_property_transformed
+        basis_transform = grouped_property_transformed.get_property("ElectronicBasisTransform")
+        e_energy = grouped_property_transformed.get_property("ElectronicEnergy")
+
+        new_coeff_alpha = np.matmul(basis_transform.coeff_alpha, matrix_a)
+        new_coeff_beta = np.matmul(basis_transform.coeff_beta, matrix_b)
 
         # use ElectronicBasisTransform
-        transform = ElectronicBasisTransform(
-            ElectronicBasis.MO, ElectronicBasis.MO, matrix_a, matrix_b
+        new_basis_transform = ElectronicBasisTransform(
+            ElectronicBasis.AO, ElectronicBasis.MO, new_coeff_alpha, new_coeff_beta
         )
 
         # only 1 & 2 body integrals have the "transform_basis" method,
         # so I access them through the electronic energy
-        e_energy = grouped_property_transformed.get_property("ElectronicEnergy")
-        one_body_integrals = e_energy.get_electronic_integral(ElectronicBasis.MO, 1)
-        two_body_integrals = e_energy.get_electronic_integral(ElectronicBasis.MO, 2)
+        one_body_integrals_ao = e_energy.get_electronic_integral(ElectronicBasis.AO, 1)
+        two_body_integrals_ao = e_energy.get_electronic_integral(ElectronicBasis.AO, 2)
 
-        # the basis transform should be applied in place, but it's not???
-        # unless I manually add the integrals, the result of second_q_ops()
-        # doesn't change.
-        # I have to look further into this.
-        e_energy.add_electronic_integral(one_body_integrals.transform_basis(transform))
-        e_energy.add_electronic_integral(two_body_integrals.transform_basis(transform))
+        one_body_integrals_mo_rot = one_body_integrals_ao.transform_basis(new_basis_transform)
+        two_body_integrals_mo_rot = two_body_integrals_ao.transform_basis(new_basis_transform)
+
+        e_energy.add_electronic_integral(one_body_integrals_mo_rot)
+        e_energy.add_electronic_integral(two_body_integrals_mo_rot)
 
         # after applying the rotation, recompute operator
         rotated_main_second_q_op = e_energy.second_q_ops()

qiskit_nature/algorithms/ground_state_solvers/oovqe_solver.py
@@ -42,6 +42,7 @@ def energy_evaluation_oo(
     ground_state_eigensolver, solver, parameters: np.ndarray
 ) -> Union[float, List[float]]:
     """Doctstring"""
+    print("parameters: ", parameters)
     num_parameters_ansatz = solver.ansatz.num_parameters
     if num_parameters_ansatz == 0:
         raise RuntimeError("The ansatz must be parameterized, but has 0 free parameters.")
@@ -51,18 +52,21 @@ def energy_evaluation_oo(
     ansatz_parameter_values = parameters[:num_parameters_ansatz]
     rotation_parameter_values = parameters[num_parameters_ansatz:]  # is this the correct order?
 
+    print('Parameters of wavefunction are: \n%s', repr(ansatz_parameter_values))
+    print('Parameters of orbital rotation are: \n%s', repr(rotation_parameter_values))
+
     # CALCULATE COEFFICIENTS OF ROTATION MATRIX HERE:
-    # matrix_a, matrix_b = np.eye(2), np.eye(2)
     matrix_a, matrix_b = ground_state_eigensolver.orbital_rotation.get_orbital_rotation_matrix(
         rotation_parameter_values
     )
 
+    print("Nature matrix a: ", matrix_a)
+    print("Nature matrix b: ", matrix_b)
     # ROTATE AND RECOMPUTE OPERATOR HERE:
-    # what about aux_ops??? They should be rotated too.
-    # Not implemented yet.
-
+    # what about aux_ops???
     rotated_operator = ground_state_eigensolver.rotate_orbitals(matrix_a, matrix_b)
 
+    print("Rotated operator: ", rotated_operator[:5])
     # use rotated operator for constructing expect_op
     expect_op, expectation = solver.construct_expectation(
         ansatz_params, rotated_operator, return_expectation=True
@@ -88,6 +92,12 @@ def energy_evaluation_oo(
         solver._eval_count += len(means)
 
     end_time = time()
+    print(
+        "Energy evaluation returned %s - %.5f (ms), eval count: %s",
+        means,
+        (end_time - start_time) * 1000,
+        solver._eval_count,
+    )
     logger.info(
         "Energy evaluation returned %s - %.5f (ms), eval count: %s",
         means,
@@ -115,23 +125,25 @@ def compute_minimum_eigenvalue_oo(
     # validation
     solver._check_operator_ansatz(operator)
 
-    # set an expectation for this algorithm run (will be reset to None at the end)
-    initial_point_ansatz = _validate_initial_point(solver.initial_point, solver.ansatz)
-    bounds_ansatz = _validate_bounds(solver.ansatz)
+    # # set an expectation for this algorithm run (will be reset to None at the end)
+    # initial_point_ansatz = _validate_initial_point(solver.initial_point, solver.ansatz)
+    # bounds_ansatz = _validate_bounds(solver.ansatz)
+    #
+    # # HERE: the real initial point and bounds include the ansatz and the oo parameters:
+    # bounds_oo_val: tuple = (-2 * np.pi, 2 * np.pi)
+    # initial_pt_scalar: float = 1e-1
+    #
+    # initial_point_oo = np.asarray(
+    #     [initial_pt_scalar for _ in range(ground_state_eigensolver.orbital_rotation.num_parameters)]
+    # )
+    # bounds_oo = np.asarray(
+    #     [bounds_oo_val for _ in range(ground_state_eigensolver.orbital_rotation.num_parameters)]
+    # )
 
-    # HERE: the real initial point and bounds include the ansatz and the oo parameters:
-    bounds_oo_val: tuple = (-2 * np.pi, 2 * np.pi)
-    initial_pt_scalar: float = 1e-1
+    initial_point = ground_state_eigensolver.initial_point
+    bounds = ground_state_eigensolver.bounds
 
-    initial_point_oo = np.asarray(
-        [initial_pt_scalar for _ in range(ground_state_eigensolver.orbital_rotation.num_parameters)]
-    )
-    bounds_oo = np.asarray(
-        [bounds_oo_val for _ in range(ground_state_eigensolver.orbital_rotation.num_parameters)]
-    )
-
-    initial_point = np.concatenate((initial_point_ansatz, initial_point_oo))
-    bounds = np.concatenate((bounds_ansatz, bounds_oo))
+    # np.concatenate((bounds_ansatz, bounds_oo))
 
     # HERE: for the moment, not taking care of aux_operators
     # Does the orbital rotation affect them???
@@ -216,6 +228,7 @@ def compute_minimum_eigenvalue_oo(
     # TODO delete as soon as get_optimal_vector etc are removed
     solver._ret = result
 
+    print("result: ", result)
     if aux_operators is not None:
         # construct expectation AFTER optimization loop is finished, with rotated operator
         rotated_operator = operator

qiskit_nature/properties/second_quantization/electronic/integrals/one_body_electronic_integrals.py
@@ -22,7 +22,7 @@ from qiskit_nature import QiskitNatureError
 
 from .electronic_integrals import ElectronicIntegrals
 from ..bases import ElectronicBasis, ElectronicBasisTransform
-
+from ..bases.electronic_rotation_transform import ElectronicRotationTransform
 
 class OneBodyElectronicIntegrals(ElectronicIntegrals):
     """The 1-body electronic integrals."""
@@ -70,8 +70,8 @@ class OneBodyElectronicIntegrals(ElectronicIntegrals):
             QiskitNatureError: if the integrals do not match
                 :class:`~qiskit_nature.properties.second_quantization.electronic.bases.ElectronicBasisTransform.initial_basis`.
         """
-        if self._basis == transform.final_basis:
-            return self
+        # if self._basis == transform.final_basis:
+        #     return self
 
         if self._basis != transform.initial_basis:
             raise QiskitNatureError(
@@ -85,7 +85,27 @@ class OneBodyElectronicIntegrals(ElectronicIntegrals):
             matrix_b = np.dot(
                 np.dot(transform.coeff_beta.T, self.get_matrix(1)), transform.coeff_beta
             )
+
         return OneBodyElectronicIntegrals(transform.final_basis, (matrix_a, matrix_b))
+
+    # def rotate_orbitals(self, transform: ElectronicRotationTransform) -> OneBodyElectronicIntegrals:
+    #
+    #     if self._basis != transform.basis:
+    #         raise QiskitNatureError(
+    #             f"The integrals' basis, {self._basis}, does not match the initial basis of the "
+    #             f"rotation transform, {transform.initial_basis}."
+    #         )
+    #
+    #     print("mo coeff nature: ", self._matrices[0])
+    #     matrix_a = np.matmul(self._matrices[0], transform.coeff_alpha)
+    #     matrix_b = None
+    #     if self._matrices[1] is not None or not transform.is_alpha_equal_beta():
+    #         matrix_b = np.matmul(self.get_matrix(1), transform.coeff_beta)
+    #
+    #     print("mo coeff nature after: ", matrix_a)
+    #
+    #     return OneBodyElectronicIntegrals(transform.basis, (matrix_a, matrix_b))
+
 
     def to_spin(self) -> np.ndarray:
         """Transforms the integrals into the special