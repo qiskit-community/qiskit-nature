eeee35a868a201aa9a414231bda9f675d10cee19
MarcDrudis <MarcSanzDrudis@outlook.com>
2022-06-14 11:37:24 +0200

    Trailing Whitespace

--- test/results/utils/test_protein_shape_file_gen.py
+++ test/results/utils/test_protein_shape_file_gen.py
@@ -144,4 +144,4 @@ class TestProteinShapeFileGen(QiskitNatureTestCase):
                 file_temp = os.path.join(tmpdirname, name_file + "_temp.xyz")
                 self.assertTrue(filecmp.cmp(file_temp, file_test))
 
-            
+
