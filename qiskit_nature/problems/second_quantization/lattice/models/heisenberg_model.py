472a24e5bea512015d7ee7adf511fadcd2404a2d
José Victor <66651520+jvscursulim@users.noreply.github.com>
2022-07-19 17:19:13 -0300

    Update qiskit_nature/problems/second_quantization/lattice/models/heisenberg_model.py
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>

--- qiskit_nature/problems/second_quantization/lattice/models/heisenberg_model.py
+++ qiskit_nature/problems/second_quantization/lattice/models/heisenberg_model.py
@@ -10,7 +10,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"The Heisenberg model"
+"""The Heisenberg model."""
+
 import logging
 from typing import Optional
 from fractions import Fraction
