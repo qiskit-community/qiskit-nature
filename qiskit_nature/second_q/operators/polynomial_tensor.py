2d32bf8c6f7dcf8847802d535184b47b1571ab2e
SaashaJoshi <saashajoshi@ibm.com>
2022-08-10 15:17:53 -0400

    updating polynomial_tensor and unittest

--- qiskit_nature/second_q/operators/polynomial_tensor.py
+++ qiskit_nature/second_q/operators/polynomial_tensor.py
@@ -13,7 +13,7 @@
 """Polynomial Tensor class"""
 
 from __future__ import annotations
-from typing import Dict
+from typing import Dict, Mapping
 from numbers import Number
 import numpy as np
 from qiskit.quantum_info.operators.mixins import (
@@ -26,31 +26,35 @@ from qiskit.quantum_info.operators.mixins import (
 class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
     """Polynomial Tensor class"""
 
-    def __init__(self, data: Dict[str, np.ndarray]):
-        if not isinstance(data, Dict):
-            raise TypeError(f"data {data} must be a Dictionary")
-
-        self._data = data
+    def __init__(self, data: Mapping[str, np.ndarray | Number]):
+        copy_dict: Dict[str, np.ndarray] = {}
 
         shapes = set()
-        for key, value in self._data.items():
+        for key, value in data.items():
+            if isinstance(value, Number):
+                value = np.asarray(value)
+
             if len(value.shape) != len(key):
                 raise ValueError(
-                    f"data key {key} of length {len(key)} does not match "
+                    f"Data key {key} of length {len(key)} does not match "
                     f"data value matrix of dimensions {value.shape}"
                 )
 
-            shape = set(value.shape)
+            dims = set(value.shape)
 
-            if len(shape) != 1:
+            if len(dims) > 1:
                 raise ValueError(
                     f"For key {key}: dimensions of value matrix are not identical {value.shape}"
                 )
 
-            shapes.update(shape)
+            shapes.update(dims)
+            copy_dict[key] = value
 
         if len(shapes) != 1:
+            print(shapes)
             raise ValueError("Dimensions of value matrices in data dictionary are not identical.")
+
+        self._data = copy_dict
 
     def mul(self, other: complex):
         """Scalar multiplication of PolynomialTensor with complex"""
@@ -58,9 +62,9 @@ class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
         if not isinstance(other, Number):
             raise TypeError(f"other {other} must be a number")
 
-        prod_dict = {}
+        prod_dict: Dict[str, np.ndarray] = {}
         for key, matrix in self._data.items():
-            prod_dict[key] = matrix * other
+            prod_dict[key] = np.multiply(matrix, other)
         return PolynomialTensor(prod_dict)
 
     def _multiply(self, other):
@@ -72,7 +76,8 @@ class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
         if not isinstance(other, PolynomialTensor):
             raise TypeError("Incorrect argument type: other should be PolynomialTensor")
 
-        sum_dict = self._data.copy()
+        # Copy values from data to sum dict one by one.
+        sum_dict: Dict[str, np.ndarray] = self._data.copy()
         for other_key, other_value in other._data.items():
             if other_key in sum_dict.keys():
                 if other_value.shape == np.shape(sum_dict[other_key]):
@@ -96,8 +101,8 @@ class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
         """Check equality of PolynomialTensors"""
 
         if self._data.keys() == other._data.keys():
-            for key in self._data.keys():
-                if np.allclose(self._data[key], other._data[key], atol=self.atol, rtol=self.rtol):
+            for key, value in self._data.items():
+                if np.allclose(value, other._data[key], atol=self.atol, rtol=self.rtol):
                     return isinstance(other, PolynomialTensor)
                 else:
                     return False
@@ -107,7 +112,7 @@ class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
     def conjugate(self):
         """Conjugate of PolynomialTensors"""
 
-        conj_dict = {}
+        conj_dict: Dict[str, np.ndarray] = {}
         for key, value in self._data.items():
             conj_dict[key] = np.conjugate(value)
 
@@ -116,7 +121,7 @@ class PolynomialTensor(LinearMixin, AdjointMixin, TolerancesMixin):
     def transpose(self):
         """Transpose of PolynomialTensor"""
 
-        transpose_dict = {}
+        transpose_dict: Dict[str, np.ndarray] = {}
         for key, value in self._data.items():
             transpose_dict[key] = np.transpose(value)
 
--- test/second_q/operators/test_polynomial_tensor.py
+++ test/second_q/operators/test_polynomial_tensor.py
@@ -12,43 +12,13 @@
 
 """Test for Polynomial Tensor"""
 
+from __future__ import annotations
 import unittest
-from typing import Dict
 from test import QiskitNatureTestCase
-from ddt import ddt, data
+from ddt import ddt, idata
 import numpy as np
 from qiskit_nature.second_q.operators import PolynomialTensor
 
-
-def gen_expected_prod_poly(og_poly, dim_size, other):
-    """Generate expected product Polynomial Tensors"""
-
-    expected_poly = {}
-    for key, value in og_poly.items():
-        num_dim = len(key)
-        expected_poly[key] = (np.arange(1, dim_size ** num_dim + 1) * other).reshape((dim_size,) * num_dim)
-
-    return expected_poly
-
-def gen_expected_transpose_poly(og_poly, dim_size):
-    """Generate expected transpose of Polynomial Tensor"""
-
-    expected_poly = {}
-    for key, value in og_poly.items():
-        num_dim = len(key)
-        expected_poly[key] = np.arange(1, dim_size ** num_dim + 1).reshape((dim_size,) * num_dim).transpose()
-
-    return expected_poly
-
-def gen_expected_conjugate_poly(og_poly, dim_size):
-    """Generate expected transpose of Polynomial Tensor"""
-
-    expected_poly = {}
-    for key, value in og_poly.items():
-        num_dim = len(key)
-        expected_poly[key] = np.arange(1, dim_size ** num_dim + 1).reshape((dim_size,) * num_dim).conjugate()
-
-    return expected_poly
 
 @ddt
 class TestPolynomialTensor(QiskitNatureTestCase):
@@ -57,38 +27,74 @@ class TestPolynomialTensor(QiskitNatureTestCase):
     def setUp(self) -> None:
         super().setUp()
 
-        self.og_poly: Dict[str, np.ndarray] = {
-            "+-": np.arange(1, 17).reshape(4, 4),
-            "+++-": np.arange(1, 257).reshape(4, 4, 4, 4),
+        self.og_poly = {
+            "": 1.0,
+            "+": self.build_matrix(4, 1),
+            "+-": self.build_matrix(4, 2),
+            "++--": self.build_matrix(4, 4),
         }
 
-        for value in self.og_poly.values():
-            self.dim_size = np.shape(value)
-
-        self.expected_sum_poly: Dict[str, np.ndarray] = {
-            "+-": (np.arange(1, 17) * 2).reshape(4, 4),
-            "+++-": (np.arange(1, 257) * 2).reshape(4, 4, 4, 4),
+        self.expected_conjugate_poly = {
+            "": 1.0,
+            "+": self.build_matrix(4, 1).conjugate(),
+            "+-": self.build_matrix(4, 2).conjugate(),
+            "++--": self.build_matrix(4, 4).conjugate(),
         }
 
+        self.expected_transpose_poly = {
+            "": 1.0,
+            "+": self.build_matrix(4, 1).transpose(),
+            "+-": self.build_matrix(4, 2).transpose(),
+            "++--": self.build_matrix(4, 4).transpose(),
+        }
 
-    def test_init_dict(self):
-        """Test for input type in Polynomial Tensor class"""
+        self.expected_sum_poly = {
+            "": 2.0,
+            "+": np.add(self.build_matrix(4, 1), self.build_matrix(4, 1)),
+            "+-": np.add(self.build_matrix(4, 2), self.build_matrix(4, 2)),
+            "++--": np.add(self.build_matrix(4, 4), self.build_matrix(4, 4)),
+        }
 
-        if self.assertRaises(TypeError):
-            PolynomialTensor(self.og_poly)
+    @staticmethod
+    def build_matrix(dim_size, num_dim, val=1):
+        """Build dictionary value matrix"""
 
-    def test_init_dimensions(self):
-        """Test for input matrix dimensions in Polynomial Tensor"""
+        return (np.arange(1, dim_size**num_dim + 1) * val).reshape((dim_size,) * num_dim)
 
-        if self.assertRaisesRegex(ValueError, "Dimensions of value matrices in data dictionary are not identical."):
-            PolynomialTensor(self.og_poly)
+    def test_init_val_dimen(self):
+        """Test for value dimensions of data input"""
 
-    @data(2, 3, 4)
+        self.assertRaisesRegex(
+            ValueError, r"For key .* dimensions of value matrix are not identical \d+"
+        )
+
+    def test_init_key_len(self):
+        """Test for key length and value dimensions of data input"""
+
+        self.assertRaisesRegex(
+            ValueError,
+            r"Data key .* of length \d does not match data value matrix of dimensions \((\d+), (\d+)\)",
+        )
+
+    def test_init_all_val_dimen(self):
+        """Test for dimesnions of all values of data input"""
+
+        self.assertRaisesRegex(
+            ValueError, r"Dimensions of value matrices in data dictionary are not identical."
+        )
+
+    @idata(np.linspace(2, 3, 5))
     def test_mul(self, other):
         """Test for scalar multiplication"""
 
+        expected_prod_poly = {
+            "": 1.0 * other,
+            "+": self.build_matrix(4, 1, other),
+            "+-": self.build_matrix(4, 2, other),
+            "++--": self.build_matrix(4, 4, other),
+        }
+
         result = PolynomialTensor(self.og_poly).mul(other)
-        expected_prod_poly = gen_expected_prod_poly(self.og_poly, self.dim_size[0], other)
         self.assertEqual(result, PolynomialTensor(expected_prod_poly))
 
     def test_add(self):
@@ -101,15 +107,13 @@ class TestPolynomialTensor(QiskitNatureTestCase):
         """Test for conjugate of Polynomial Tensor"""
 
         result = PolynomialTensor(self.og_poly).conjugate()
-        expected_conjugate_poly = gen_expected_conjugate_poly(self.og_poly, self.dim_size[0])
-        self.assertEqual(result, PolynomialTensor(expected_conjugate_poly))
+        self.assertEqual(result, PolynomialTensor(self.expected_conjugate_poly))
 
     def test_transpose(self):
         """Test for transpose of Polynomial Tensor"""
 
         result = PolynomialTensor(self.og_poly).transpose()
-        expected_transpose_poly = gen_expected_transpose_poly(self.og_poly, self.dim_size[0])
-        self.assertEqual(result, PolynomialTensor(expected_transpose_poly))
+        self.assertEqual(result, PolynomialTensor(self.expected_transpose_poly))
 
 
 if __name__ == "__main__":
