923195caa3aa6374c41aa045e2727c07897c99cf
Manoel Marques <Manoel.Marques@ibm.com>
2022-09-27 10:56:12 -0400

    Enable latest pyscf (#846)

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setuptools.setup(
     include_package_data=True,
     python_requires=">=3.7",
     extras_require={
-        "pyscf": ["pyscf<2.1.1; sys_platform != 'win32' and (python_version < '3.10' or sys_platform != 'darwin')"],
+        "pyscf": ["pyscf; sys_platform != 'win32'"],
         "mpl": ["matplotlib>=3.3,<3.6"],
     },
     zip_safe=False,
