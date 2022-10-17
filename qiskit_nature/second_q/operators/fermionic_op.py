17ae2b0bf1ea324fb91af5f718c128459682af91
Kevin J. Sung <kevjsung@umich.edu>
2022-10-17 12:47:08 -0400

    revert chop

--- qiskit_nature/second_q/operators/fermionic_op.py
+++ qiskit_nature/second_q/operators/fermionic_op.py
@@ -267,7 +267,7 @@ class FermionicOp(SparseLabelOp):
                     for value, *index in zip(coo.data, *coo.coords):
                         data[label_template.format(*index)] = value
 
-        return cls(data, copy=False, num_spin_orbitals=tensor.register_length)
+        return cls(data, copy=False, num_spin_orbitals=tensor.register_length).chop()
 
     def __repr__(self) -> str:
         data_str = f"{dict(self.items())}"
