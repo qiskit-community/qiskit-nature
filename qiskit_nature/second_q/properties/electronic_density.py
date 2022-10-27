c393383e1203c2da1b57c3c0c8dde9b828030796
Max Rossmannek <oss@zurich.ibm.com>
2022-10-27 13:26:19 +0200

    perf: some performance improvements for ElectronicDensity

--- qiskit_nature/second_q/properties/electronic_density.py
+++ qiskit_nature/second_q/properties/electronic_density.py
@@ -37,6 +37,8 @@ class ElectronicDensity(ElectronicIntegrals):
     def from_orbital_occupation(
         alpha_occupation: Sequence[float],
         beta_occupation: Sequence[float],
+        *,
+        include_rdm2: bool = True,
     ) -> ElectronicDensity:
         """Initializes an ``ElectronicDensity`` from the provided orbital occupations.
 
@@ -46,20 +48,25 @@ class ElectronicDensity(ElectronicIntegrals):
         Args:
             alpha_occupation: the alpha-spin orbital occupations.
             beta_occupation: the beta-spin orbital occupations.
+            include_rdm2: whether to include the 2-body RDMs.
 
         Returns:
             The resulting ``ElectronicDensity``.
         """
         rdm1_a = np.diag(alpha_occupation)
-        rdm2_aa = np.einsum("ij,kl->ijkl", rdm1_a, rdm1_a) - np.einsum(
-            "ij,kl->iklj", rdm1_a, rdm1_a
-        )
-
         rdm1_b = np.diag(beta_occupation)
-        rdm2_bb = np.einsum("ij,kl->ijkl", rdm1_b, rdm1_b) - np.einsum(
-            "ij,kl->iklj", rdm1_b, rdm1_b
-        )
-        rdm2_ba = np.einsum("ij,kl->ijkl", rdm1_a, rdm1_b)
+
+        rdm2_aa: np.ndarray | None = None
+        rdm2_bb: np.ndarray | None = None
+        rdm2_ba: np.ndarray | None = None
+        if include_rdm2:
+            rdm2_aa = np.einsum("ij,kl->ijkl", rdm1_a, rdm1_a) - np.einsum(
+                "ij,kl->iklj", rdm1_a, rdm1_a
+            )
+            rdm2_bb = np.einsum("ij,kl->ijkl", rdm1_b, rdm1_b) - np.einsum(
+                "ij,kl->iklj", rdm1_b, rdm1_b
+            )
+            rdm2_ba = np.einsum("ij,kl->ijkl", rdm1_a, rdm1_b)
 
         return ElectronicDensity.from_raw_integrals(
             rdm1_a, rdm2_aa, rdm1_b, rdm2_bb, rdm2_ba, auto_index_order=False
@@ -73,6 +80,7 @@ class ElectronicDensity(ElectronicIntegrals):
         """
         tensor = self.second_q_coeffs()
         register_length = tensor.register_length
+        half = register_length // 2
 
         aux_ops = {}
         for key in tensor:
@@ -83,6 +91,9 @@ class ElectronicDensity(ElectronicIntegrals):
 
             ndarray = cast(np.ndarray, tensor[key])
             for index in np.ndindex(*ndarray.shape):
+                if not _filter_index(index, half):
+                    continue
+
                 aux_ops[f"RDM{index}"] = FermionicOp(
                     {label_template.format(*index): 1.0},
                     num_spin_orbitals=register_length,
@@ -103,13 +114,22 @@ class ElectronicDensity(ElectronicIntegrals):
             result: the result to add meaning to.
         """
         n_spatial = self.register_length
-        n_spin = 2 * n_spatial
-
-        rdm1 = np.zeros((n_spin, n_spin), dtype=float)
-        rdm2 = np.zeros((n_spin, n_spin, n_spin, n_spin), dtype=float)
 
         rdm1_idx_regex = re.compile(r"RDM\((\d+), (\d+)\)")
         rdm2_idx_regex = re.compile(r"RDM\((\d+), (\d+), (\d+), (\d+)\)")
+
+        contains_rdm2 = "++--" in self.alpha.keys()
+
+        rdm1_a = np.zeros((n_spatial, n_spatial), dtype=float)
+        rdm1_b = np.zeros((n_spatial, n_spatial), dtype=float)
+
+        rdm2_aa: np.ndarray | None = None
+        rdm2_bb: np.ndarray | None = None
+        rdm2_ba: np.ndarray | None = None
+        if contains_rdm2:
+            rdm2_aa = np.zeros((n_spatial, n_spatial, n_spatial, n_spatial), dtype=float)
+            rdm2_bb = np.zeros((n_spatial, n_spatial, n_spatial, n_spatial), dtype=float)
+            rdm2_ba = np.zeros((n_spatial, n_spatial, n_spatial, n_spatial), dtype=float)
 
         for name, aux_value in result.aux_operators_evaluated[0].items():
             # immediately skip zero values
@@ -118,20 +138,54 @@ class ElectronicDensity(ElectronicIntegrals):
 
             match = rdm1_idx_regex.fullmatch(name)
             if match is not None:
-                mo_i, mo_j = (int(idx) for idx in match.groups())
-                rdm1[mo_i, mo_j] = aux_value.real
+                index = tuple(int(idx) for idx in match.groups())
+                bools = _boolean_index(index, n_spatial)
+
+                if all(bools):
+                    rdm1_a[index] = aux_value.real
+                elif not any(bools):
+                    rdm1_b[_shifted_index(index, n_spatial)] = aux_value.real
+
                 continue
 
-            match = rdm2_idx_regex.fullmatch(name)
-            if match is not None:
-                mo_i, mo_j, mo_k, mo_l = (int(idx) for idx in match.groups())
-                rdm2[mo_i, mo_j, mo_k, mo_l] = aux_value.real
+            if contains_rdm2:
+                match = rdm2_idx_regex.fullmatch(name)
+                if match is not None:
+                    index = tuple(int(idx) for idx in match.groups())
+                    bools = _boolean_index(index, n_spatial)
+
+                    if all(bools):
+                        rdm2_aa[index] = aux_value.real
+                    elif not any(bools):
+                        rdm2_bb[_shifted_index(index, n_spatial)] = aux_value.real
+                    elif bools[0] and bools[3] and not bools[1] and not bools[2]:
+                        rdm2_ba[_shifted_index(index, n_spatial)] = aux_value.real
 
         result.electronic_density = ElectronicDensity.from_raw_integrals(
-            rdm1[n_spatial:, n_spatial:],
-            rdm2[n_spatial:, n_spatial:, n_spatial:, n_spatial:],
-            rdm1[:n_spatial, :n_spatial],
-            rdm2[:n_spatial, :n_spatial, :n_spatial, :n_spatial],
-            rdm2[:n_spatial, n_spatial:, n_spatial:, :n_spatial],
-            auto_index_order=False,
+            rdm1_a, rdm2_aa, rdm1_b, rdm2_bb, rdm2_ba, auto_index_order=False
         )
+
+
+def _boolean_index(index: tuple[int, ...], size: int) -> tuple[bool, ...]:
+    return tuple(i < size for i in index)
+
+
+def _shifted_index(index: tuple[int, ...], size: int) -> tuple[int, ...]:
+    bools = _boolean_index(index, size)
+    return tuple(idx if bools[i] else idx - size for i, idx in enumerate(index))
+
+
+def _filter_index(index: tuple[int, ...], size: int) -> bool:
+    bools = _boolean_index(index, size)
+    nbody = len(index)
+
+    if nbody == 2:
+        return all(bools) or not any(bools)
+
+    if nbody == 4:
+        return (
+            all(bools)  # alpha-alpha
+            or not any(bools)  # beta-beta
+            or (bools[0] and bools[3] and not bools[1] and not bools[2])  # beta-alpha
+        )
+    return False
