01533bb06feb17ec15ab72e2aec03efd93487f21
Dariusz Lasecki <dal@zurich.ibm.com>
2022-11-03 15:23:18 +0100

    Update qiskit_nature/second_q/algorithms/excited_states_solvers/excited_states_solver.py
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>

--- qiskit_nature/second_q/algorithms/excited_states_solvers/excited_states_solver.py
+++ qiskit_nature/second_q/algorithms/excited_states_solvers/excited_states_solver.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" The excited states calculation interface. """
+"""The excited states calculation interface."""
 
 from __future__ import annotations
 
