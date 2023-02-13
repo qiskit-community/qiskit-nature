5a6fd2f2c9e999300055db0a7a89bd1ae99144d9
DeLicht <ukskosana@gmail.com>
2023-02-13 11:18:52 +0200

    Update test/second_q/hamiltonians/lattices/test_kagome_lattice.py
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>

--- test/second_q/hamiltonians/lattices/test_kagome_lattice.py
+++ test/second_q/hamiltonians/lattices/test_kagome_lattice.py
@@ -63,26 +63,7 @@ class TestKagomeLattice(QiskitNatureTestCase):
             (13, 15, 1 - 1j),
         ]
 
-        self.weighted_self_loops = [
-            (0, 0, 0.0),
-            (1, 1, 0.0),
-            (2, 2, 0.0),
-            (3, 3, 0.0),
-            (4, 4, 0.0),
-            (5, 5, 0.0),
-            (6, 6, 0.0),
-            (7, 7, 0.0),
-            (8, 8, 0.0),
-            (9, 9, 0.0),
-            (10, 10, 0.0),
-            (11, 11, 0.0),
-            (12, 12, 0.0),
-            (13, 13, 0.0),
-            (14, 14, 0.0),
-            (15, 15, 0.0),
-            (16, 16, 0.0),
-            (17, 17, 0.0),
-        ]
+        self.weighted_self_loops = [(i, i, 0.0) for i in range(18)]
 
         self.weighted_boundary_edges = [
             (7, 0, 1 + 1j),
