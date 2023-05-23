f67eb91dc9607d9be42df7b43ede93aca96cfcd2
Francesco <80473219+ftroisi@users.noreply.github.com>
2023-05-23 09:15:17 +0200

    Bosonic operators (#1085)
    * Created bosonic operator file. Some notes on the code
    
    * modified ordering functions using commutator instead of anti-commutator
    
    * Created simplify method
    
    * Created test class for bosonic operator
    
    * Moved normal_order and index_order before simplify. Fixed the expected results
    
    * Fixed tests. Hermicity test is still missing
    
    * Improved algorithm for simplify. Now calls normal order as first operation. Fixed tests
    
    * fixed is_hermitian method
    
    * Addressed PR comments
    
    * Pipelines fixes
    
    * Created bosonic_linear_mapper class
    
    * Added some unit tests for bosonic linear mapper. Pipelines fixes
    
    * Removed trailing whitspace
    
    * Fixed test case +_0 -_0 with truncation 2
    
    * Implemented test cases +_0 -_1 and -_1 +_0. Some CI fixes
    
    * Added test case +_0 +_0
    
    * Some CI fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>
    
    * Added test for commutator relation
    
    * Added terms to pylintdict
    
    * Updated some mapper tests
    
    * Addressed some PR comments. Fixed final tests
    
    * Addressed PR comments for docstrings
    
    * Further doc fixes
    
    * Further CI fixes
    
    * Added release note
    
    * Spell fix in release note
    
    * Added methods from_terms and _permute_terms
    
    * Addressed PR comments
    
    * ran make black
    
    * Added r in front of docstring
    
    * Added reference for bosonic op truncation
    
    * Addressed PR comments
    
    * Added mapper example in bosonicLinearMapper docString
    
    * Updated error message
    
    * Renamed truncation property. Defined its setter
    
    * Apply suggestions for max_occupation setter
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    
    * ran make black
    
    * Removed unused import
    
    * Doc fixes
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    
    * Doc fix
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    
    * ran make black
    
    * Doc fix
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    
    * ran make black
    
    * Apply suggestions from code review
    
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>
    
    * Minor doc fix
    
    * Apply suggestions from code review
    
    * Update qiskit_nature/second_q/mappers/bosonic_mapper.py
    
    ---------
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
    Co-authored-by: Max Rossmannek <max.rossmannek@uzh.ch>

--- qiskit_nature/second_q/mappers/__init__.py
+++ qiskit_nature/second_q/mappers/__init__.py
@@ -17,7 +17,8 @@ Mappers (:mod:`qiskit_nature.second_q.mappers`)
 
 .. currentmodule:: qiskit_nature.second_q.mappers
 
-The classes here are used to convert fermionic, vibrational and spin operators to qubit operators.
+The classes here are used to convert fermionic, bosonic, vibrational and spin operators to qubit
+operators.
 
 .. autosummary::
    :toctree: ../stubs/
@@ -49,6 +50,17 @@ blocked) order, you can use the following wrapper:
    :nosignatures:
 
    InterleavedQubitMapper
+
+
+BosonicOp Mappers
++++++++++++++++++++
+
+.. autosummary::
+   :toctree: ../stubs/
+   :template: autosummary/class_with_inherited_members.rst
+   :nosignatures:
+
+   BosonicLinearMapper
 
 VibrationalOp Mappers
 +++++++++++++++++++++
@@ -102,6 +114,7 @@ from .bravyi_kitaev_mapper import BravyiKitaevMapper
 from .jordan_wigner_mapper import JordanWignerMapper
 from .parity_mapper import ParityMapper
 from .linear_mapper import LinearMapper
+from .bosonic_linear_mapper import BosonicLinearMapper
 from .logarithmic_mapper import LogarithmicMapper
 from .direct_mapper import DirectMapper
 from .qubit_mapper import QubitMapper
@@ -116,6 +129,7 @@ __all__ = [
     "JordanWignerMapper",
     "ParityMapper",
     "LinearMapper",
+    "BosonicLinearMapper",
     "LogarithmicMapper",
     "QubitConverter",
     "QubitMapper",
---
+++ test/second_q/mappers/test_bosonic_linear_mapper.py
@@ -0,0 +1,190 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test Bosonic Linear Mapper """
+
+import unittest
+
+from test import QiskitNatureTestCase
+
+from ddt import ddt, data, unpack
+import numpy as np
+
+from qiskit.quantum_info import SparsePauliOp
+from qiskit_nature.second_q.operators import BosonicOp
+from qiskit_nature.second_q.mappers import BosonicLinearMapper
+from qiskit_nature import settings
+
+
+@ddt
+class TestBosonicLinearMapper(QiskitNatureTestCase):
+    """Test Bosonic Linear Mapper"""
+
+    # Define some useful coefficients
+    sq_2 = np.sqrt(2)
+
+    bos_op1 = BosonicOp({"+_0": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op1_tr1 = SparsePauliOp(["XX", "YY", "YX", "XY"], coeffs=[0.25, 0.25, -0.25j, 0.25j])
+    # Using: max_occupation = 2
+    ref_qubit_op1_tr2 = SparsePauliOp(
+        ["IXX", "IYY", "IYX", "IXY", "XXI", "YYI", "YXI", "XYI"],
+        coeffs=[0.25, 0.25, -0.25j, 0.25j, sq_2 / 4, sq_2 / 4, -1j * sq_2 / 4, 1j * sq_2 / 4],
+    )
+
+    bos_op2 = BosonicOp({"-_0": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op2_tr1 = SparsePauliOp(["XX", "YY", "YX", "XY"], coeffs=[0.25, 0.25, 0.25j, -0.25j])
+    # Using: max_occupation = 2
+    ref_qubit_op2_tr2 = SparsePauliOp(
+        ["IXX", "IYY", "IYX", "IXY", "XXI", "YYI", "YXI", "XYI"],
+        coeffs=[0.25, 0.25, 0.25j, -0.25j, sq_2 / 4, sq_2 / 4, 1j * sq_2 / 4, -1j * sq_2 / 4],
+    )
+
+    bos_op3 = BosonicOp({"+_1": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op3_tr1 = SparsePauliOp(
+        ["XXII", "YYII", "YXII", "XYII"], coeffs=[0.25, 0.25, -0.25j, 0.25j]
+    )
+    # Using: max_occupation = 2
+    ref_qubit_op3_tr2 = SparsePauliOp(
+        ["IXXIII", "IYYIII", "IYXIII", "IXYIII", "XXIIII", "YYIIII", "YXIIII", "XYIIII"],
+        coeffs=[0.25, 0.25, -0.25j, 0.25j, sq_2 / 4, sq_2 / 4, -1j * sq_2 / 4, 1j * sq_2 / 4],
+    )
+
+    bos_op4 = BosonicOp({"-_1": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op4_tr1 = SparsePauliOp(
+        ["XXII", "YYII", "YXII", "XYII"], coeffs=[0.25, 0.25, 0.25j, -0.25j]
+    )
+    # Using: max_occupation = 2
+    ref_qubit_op4_tr2 = SparsePauliOp(
+        ["IXXIII", "IYYIII", "IYXIII", "IXYIII", "XXIIII", "YYIIII", "YXIIII", "XYIIII"],
+        coeffs=[0.25, 0.25, 0.25j, -0.25j, sq_2 / 4, sq_2 / 4, 1j * sq_2 / 4, -1j * sq_2 / 4],
+    )
+
+    bos_op5 = BosonicOp({"+_0 -_0": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op5_tr1 = SparsePauliOp(["II", "ZZ", "ZI", "IZ"], coeffs=[0.25, -0.25, -0.25, 0.25])
+    # Using: max_occupation = 2
+    ref_qubit_op5_tr2 = SparsePauliOp(
+        ["III", "IZZ", "IZI", "IIZ", "ZZI", "ZII"], coeffs=[0.75, -0.25, 0.25, 0.25, -0.5, -0.5]
+    )
+
+    bos_op6 = BosonicOp({"-_0 +_0": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op6_tr1 = SparsePauliOp(["II", "ZZ", "ZI", "IZ"], coeffs=[0.25, -0.25, +0.25, -0.25])
+    # Using: max_occupation = 2
+    ref_qubit_op6_tr2 = SparsePauliOp(
+        ["III", "IZZ", "IZI", "IIZ", "ZZI", "ZII"], coeffs=[0.75, -0.25, -0.25, -0.25, -0.5, 0.5]
+    )
+
+    bos_op7 = BosonicOp({"+_0 -_1": 1})
+    bos_op8 = BosonicOp({"-_1 +_0": 1})
+    # Using: max_occupation = 1
+    # fmt: off
+    ref_qubit_op7_8_tr1 = SparsePauliOp(
+        ["XXXX", "XXYY", "XXYX", "XXXY", "YYXX", "YYYY", "YYYX", "YYXY",
+         "YXXX", "YXYY", "YXYX", "YXXY", "XYXX", "XYYY", "XYYX", "XYXY",],
+        coeffs=[
+         1 / 16, 1 / 16, -1j / 16, 1j / 16, 1 / 16, 1 / 16, -1j / 16, 1j / 16,
+         1j / 16, 1j / 16, 1 / 16, -1 / 16, -1j / 16, -1j / 16, -1 / 16, 1 / 16,]
+    )
+    # fmt: on
+    # Using: max_occupation = 2
+    # fmt: off
+    ref_qubit_op7_8_tr2 = SparsePauliOp(
+        ["IXXIXX", "IXXIYY", "IXXIYX", "IXXIXY", "IXXXXI", "IXXYYI", "IXXYXI", "IXXXYI",
+         "IYYIXX", "IYYIYY", "IYYIYX", "IYYIXY", "IYYXXI", "IYYYYI", "IYYYXI", "IYYXYI",
+         "IYXIXX", "IYXIYY", "IYXIYX", "IYXIXY", "IYXXXI", "IYXYYI", "IYXYXI", "IYXXYI",
+         "IXYIXX", "IXYIYY", "IXYIYX", "IXYIXY", "IXYXXI", "IXYYYI", "IXYYXI", "IXYXYI",
+         "XXIIXX", "XXIIYY", "XXIIYX", "XXIIXY", "XXIXXI", "XXIYYI", "XXIYXI", "XXIXYI",
+         "YYIIXX", "YYIIYY", "YYIIYX", "YYIIXY", "YYIXXI", "YYIYYI", "YYIYXI", "YYIXYI",
+         "YXIIXX", "YXIIYY", "YXIIYX", "YXIIXY", "YXIXXI", "YXIYYI", "YXIYXI", "YXIXYI",
+         "XYIIXX", "XYIIYY", "XYIIYX", "XYIIXY", "XYIXXI", "XYIYYI", "XYIYXI", "XYIXYI",],
+        coeffs=[
+         1 / 16, 1 / 16, -1j / 16, 1j / 16, sq_2 / 16, sq_2 / 16, -sq_2 * 1j / 16, sq_2 * 1j / 16,
+         1 / 16, 1 / 16, -1j / 16, 1j / 16, sq_2 / 16, sq_2 / 16, -sq_2 * 1j / 16, sq_2 * 1j / 16,
+         1j / 16, 1j / 16, 1 / 16, -1 / 16, sq_2 * 1j / 16, sq_2 * 1j / 16, sq_2 / 16, -sq_2 / 16,
+         -1j / 16, -1j / 16, -1 / 16, 1 / 16, -sq_2 * 1j / 16, -sq_2 * 1j / 16, -sq_2 / 16, sq_2 / 16,
+         sq_2 / 16, sq_2 / 16, -sq_2 * 1j / 16, sq_2 * 1j / 16, 2 / 16, 2 / 16, -2j / 16, 2j / 16,
+         sq_2 / 16, sq_2 / 16, -sq_2 * 1j / 16, sq_2 * 1j / 16, 2 / 16, 2 / 16, -2j / 16, 2j / 16,
+         sq_2 * 1j / 16, sq_2 * 1j / 16, sq_2 / 16, -sq_2 / 16, 2j / 16, 2j / 16, 2 / 16, -2 / 16,
+         -sq_2 * 1j / 16, -sq_2 * 1j / 16, -sq_2 / 16, sq_2 / 16, -2j / 16, -2j / 16, -2 / 16, 2 / 16,]
+    )
+    # fmt: on
+
+    bos_op9 = BosonicOp({"+_0 +_0": 1})
+    # Using: max_occupation = 1
+    ref_qubit_op9_tr1 = SparsePauliOp(["II"], coeffs=[0.0])
+    # Using: max_occupation = 2
+    ref_qubit_op9_tr2 = SparsePauliOp(
+        ["XIX", "YIX", "YIY", "XIY"], coeffs=[sq_2 / 4, -sq_2 * 1j / 4, sq_2 / 4, sq_2 * 1j / 4]
+    )
+
+    # Test max_occupation = 1
+    @data(
+        (bos_op1, ref_qubit_op1_tr1),
+        (bos_op2, ref_qubit_op2_tr1),
+        (bos_op3, ref_qubit_op3_tr1),
+        (bos_op4, ref_qubit_op4_tr1),
+        (bos_op5, ref_qubit_op5_tr1),
+        (bos_op6, ref_qubit_op6_tr1),
+        (bos_op7, ref_qubit_op7_8_tr1),
+        (bos_op8, ref_qubit_op7_8_tr1),
+        (bos_op9, ref_qubit_op9_tr1),
+    )
+    @unpack
+    def test_mapping_max_occupation_1(self, bos_op, ref_qubit_op):
+        """Test mapping to qubit operator"""
+        mapper = BosonicLinearMapper(max_occupation=1)
+        aux = settings.use_pauli_sum_op
+        try:
+            settings.use_pauli_sum_op = False
+            qubit_op = mapper.map(bos_op)
+            self.assertEqualSparsePauliOp(qubit_op, ref_qubit_op)
+        finally:
+            settings.use_pauli_sum_op = aux
+
+    @data(
+        (bos_op1, ref_qubit_op1_tr2),
+        (bos_op2, ref_qubit_op2_tr2),
+        (bos_op3, ref_qubit_op3_tr2),
+        (bos_op4, ref_qubit_op4_tr2),
+        (bos_op5, ref_qubit_op5_tr2),
+        (bos_op6, ref_qubit_op6_tr2),
+        (bos_op7, ref_qubit_op7_8_tr2),
+        (bos_op8, ref_qubit_op7_8_tr2),
+        (bos_op9, ref_qubit_op9_tr2),
+    )
+    @unpack
+    def test_mapping_max_occupation_2(self, bos_op, ref_qubit_op):
+        """Test mapping to qubit operator"""
+        mapper = BosonicLinearMapper(max_occupation=2)
+        aux = settings.use_pauli_sum_op
+        try:
+            settings.use_pauli_sum_op = False
+            qubit_op = mapper.map(bos_op)
+            self.assertEqualSparsePauliOp(qubit_op, ref_qubit_op)
+        finally:
+            settings.use_pauli_sum_op = aux
+
+    def test_error_pauli_sum_op(self):
+        """Test that if the user sets use_pauli_sum_op to true, then we return an error"""
+        mapper = BosonicLinearMapper(max_occupation=1)
+        settings.use_pauli_sum_op = True
+        with self.assertRaises(NotImplementedError):
+            mapper.map(BosonicOp({"+_0": 1}))
+
+
+if __name__ == "__main__":
+    unittest.main()
