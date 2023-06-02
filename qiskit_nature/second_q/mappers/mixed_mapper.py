92180a11c2e7506d90e04387c37570de89564d94
Anthony-Gandon <antho91370@gmail.com>
2023-06-02 09:14:08 +0200

    small fixes v2

--- qiskit_nature/second_q/mappers/mixed_mapper.py
+++ qiskit_nature/second_q/mappers/mixed_mapper.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""The Mixed mapper."""
+"""The Mixed Mapper class."""
 
 from __future__ import annotations
 
