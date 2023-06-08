22f9c16389b59acb6f88dd8971c00561cc66faf3
Max Rossmannek <oss@zurich.ibm.com>
2023-06-08 15:56:42 +0200

    fix: typing compatibility with rustworkx 0.13.0 (#1202)
    * fix: typing compatibility with rustworkx 0.13.0
    
    * fix: update copyright
    
    * hotfix: avoid mypy warning about unused ignores
    
    This change should be reverted once rustworkx 0.13.1 has fixed this
    typing incompatibility.
    
    (cherry picked from commit 08df6810ac92c972a2f220821d0b621cef08a319)

--- test/second_q/hamiltonians/lattices/test_triangular_lattice.py
+++ test/second_q/hamiltonians/lattices/test_triangular_lattice.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021, 2022.
+# (C) Copyright IBM 2021, 2023.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -14,7 +14,7 @@
 from test import QiskitNatureTestCase
 from numpy.testing import assert_array_equal
 import numpy as np
-from rustworkx import PyGraph, is_isomorphic
+from rustworkx import PyGraph, is_isomorphic  # type: ignore[attr-defined]
 from qiskit_nature.second_q.hamiltonians.lattices import (
     BoundaryCondition,
     TriangularLattice,
