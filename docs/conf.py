cb5536932cbfd6f67be10a9f7929d0337b2bf58c
Arnau Casau <47946624+arnaucasau@users.noreply.github.com>
2024-01-24 19:43:50 +0100

    Deploy docs to GitHub Pages (#1303)
    * Deploy docs to GitHub Pages
    
    * Fix conf.py copyrighht date
    
    * gettext removed

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # This code is part of a Qiskit project.
 #
-# (C) Copyright IBM 2021, 2023.
+# (C) Copyright IBM 2021, 2024.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -42,7 +42,7 @@ project = "Qiskit Nature"
 copyright = f"2018, {date.today().year}, Qiskit Nature Development Team"  # pylint: disable=redefined-builtin
 author = "Qiskit Nature Development Team"
 
-docs_url_prefix = "ecosystem/nature"
+docs_url_prefix = "qiskit-nature"
 
 # The short X.Y version
 version = qiskit_nature.__version__
@@ -159,8 +159,6 @@ translations_list = [
     ("es_UN", "Spanish"),
 ]
 language = "en"
-locale_dirs = ["locale/"]
-gettext_compact = False  # optional.
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
