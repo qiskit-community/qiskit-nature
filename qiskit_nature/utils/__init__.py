6c207a1fd74c46ffd6284c18ad8b94b0683b7783
Matteo A.C. Rossi <matteo@algorithmiq.fi>
2026-05-23 11:40:00 +0300

    fix import

--- qiskit_nature/utils/__init__.py
+++ qiskit_nature/utils/__init__.py
@@ -38,16 +38,7 @@ from .linalg import (
 )
 from .opt_einsum import get_einsum
 
-# Handles import for use of Qiskit parallel map to cater to location before
-# and after the 1.0 version
-try:
-    # From 0.46 onwards parallel map is here
-    from qiskit.utils import parallel_map as _parallel_map
-except ImportError:
-    # Until 0.46 it's here but in 0.46 raises a deprecation
-    # using it at this older location, hence we try the new
-    # location first above and only then fallback to the old here
-    from qiskit.tools import parallel_map as _parallel_map
+from qiskit.utils import parallel_map as _parallel_map
 
 __all__ = [
     "apply_matrix_to_slices",
